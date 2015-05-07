29e2cdadb2f2372fe8dda1620addbb797c179198
Renato Aguiar <renato.aguiar@hp.com>
2015-05-06 22:26:54 -0700

    Add intentcompiler service interface
    Change-Id: Iaea9b0975e2de13a95e1f274272903d905075dc7
    Signed-off-by: Renato Aguiar <renato.aguiar@hp.com>

---
+++ intentcompiler/src/main/java/org/opendaylight/nic/compiler/api/IntentCompiler.java
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this distribution,
+// and is available at http://www.eclipse.org/legal/epl-v10.html
+//------------------------------------------------------------------------------
+package org.opendaylight.nic.compiler.api;
+
+import java.util.Set;
+
+public interface IntentCompiler {
+    Set<Policy> compile(Set<Policy> policies);
+}
