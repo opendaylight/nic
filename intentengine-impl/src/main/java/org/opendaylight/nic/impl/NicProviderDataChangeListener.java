4d4afc9ed7a9129fce41b056811a07cd62fcf1b2
Ben Eze <beze@inocybe.com>
2015-05-08 13:02:42 -0400

    Add data change listener and intent capturing registry for further processing for GBP or VTN resolvers. Please do not merge
    Change-Id: I7bcb273a03b2d89ffec09cdc6da6071615443b6a
    Signed-off-by: Ben Eze <beze@inocybe.com>

---
+++ intentengine-impl/src/main/java/org/opendaylight/nic/impl/NicProviderDataChangeListener.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2015, Inocybe Technologies 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.impl;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.nic.util.IntentRunningRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NicProviderDataChangeListener implements DataChangeListener,
+	AutoCloseable {
+
+	private static final Logger LOG = LoggerFactory
+            .getLogger(NicProviderDataChangeListener.class);
+
+	
+	
+    private ListenerRegistration<DataChangeListener> intentListener = null;   
+    private IntentRunningRegistry intentRegistry;
+    private DataBroker dataProvider;
+    
+    public NicProviderDataChangeListener(DataBroker dataBroker){
+    
+    	dataProvider = dataBroker;
+    	intentRegistry = IntentRunningRegistry.getInstance();    	
+		intentListener = dataProvider.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL, NicProvider.intentNodeId, this, DataChangeScope.SUBTREE);
+    	LOG.debug("Initialized intent processor");        
+    }
+    
+    
+	@Override
+	public void close() throws Exception {
+				
+		if (intentListener != null) {
+			intentListener.close();
+        }
+		
+	}
+
+	@Override
+	public void onDataChanged(
+			AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+		
+		//Pull the list of new items
+		Map<InstanceIdentifier<?>, DataObject> createdObjects = changes.getCreatedData();
+						
+		for (Entry<InstanceIdentifier<?>, DataObject> created : createdObjects.entrySet()) {
+            if (created.getValue() != null
+                    && created.getValue() instanceof Intent) {
+            	
+            	Intent intent = (Intent) created;
+            	
+            	synchronized (intentRegistry){
+	            	LOG.debug("Intent key " + intent.getId() + " was created!");            	
+	            	try {
+						intentRegistry.addIntent(intent);
+					} catch (InterruptedException e) {
+						LOG.debug("Intent update failed - " + e.getStackTrace());
+					}
+            	}
+            }
+		}		
+		
+		//Pull the list of items that are being updated
+		Map<InstanceIdentifier<?>, DataObject> updatedObjects = changes.getUpdatedData();
+		
+		for (Entry<InstanceIdentifier<?>, DataObject> updated : updatedObjects.entrySet()) {
+            if (updated.getValue() != null
+                    && updated.getValue() instanceof Intent) {
+            	
+            	Intent intent = (Intent) updated;
+            	
+            	if(createdObjects.get(updated.getKey()) == null){
+            		
+            		synchronized (intentRegistry){
+		            	LOG.debug("Intent key " + intent.getId() + " is being updated!");	            	
+		            	try {
+							intentRegistry.updateIntent(intent);
+						} catch (InterruptedException e) {
+							LOG.debug("Intent update failed - " + e.getStackTrace());
+						}
+            		}
+            	}
+            }
+		}	
+		
+		//Pull the intents to be deleted.
+		Map<InstanceIdentifier<?>, DataObject> orginalObjects = changes.getOriginalData();
+		Set<InstanceIdentifier<?>> deletedObjects = changes.getRemovedPaths();
+		
+		for (InstanceIdentifier<?> deletedIdentifier : deletedObjects) {
+            if (deletedIdentifier.getTargetType().equals(Intent.class)) {
+            	
+            	synchronized (intentRegistry){
+	            	Intent intent = (Intent) orginalObjects.get(deletedIdentifier);
+	            	intentRegistry.removeIntent(intent);
+            	}
+            }
+		}
+		
+	}	
+	
+}
---
+++ intentengine-impl/src/main/java/org/opendaylight/nic/util/SetUtils.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.util;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Utility methods related to managing sets and maps
+ * @author readams
+ *
+ */
+public class SetUtils {
+    /**
+     * Get and/or allocate as needed a nested concurrent set inside a concurrent
+     * map in a threadsafe way.
+     * @param key the key to the concurrent map
+     * @param set the concurrent map
+     * @return the nested concurrent set
+     */
+    public static <K1, K2> Set<K2> 
+            getNestedSet(K1 key, ConcurrentMap<K1, Set<K2>> set) {
+        Set<K2> inner = set.get(key);
+        if (inner == null) {
+            inner = Collections.newSetFromMap(new ConcurrentHashMap<K2, Boolean>());
+            Set<K2> old = set.putIfAbsent(key, inner);
+            if (old != null)
+                inner = old;
+        }
+        return inner;
+    }
+}
