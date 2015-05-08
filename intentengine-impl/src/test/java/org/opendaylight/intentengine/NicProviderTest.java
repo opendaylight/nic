12bae171dc3731ec14179ae5f0f2db49a9d6e7cb
Raphael Amorim <raphael.amorim@hp.com>
2015-05-08 14:47:25 -0700

    Adding code to initialize the intents config/operational trees
    Change-Id: I587ea367511e617f57e086022adbf4c6a724363a
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

--- intentengine-impl/src/test/java/org/opendaylight/intentengine/NicProviderTest.java
+++ intentengine-impl/src/test/java/org/opendaylight/intentengine/NicProviderTest.java
@@ -4,21 +4,35 @@
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.nic.impl;
+package org.opendaylight.intentengine;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 
 public class NicProviderTest {
+
     @Test
     public void testOnSessionInitiated() throws Exception {
         try (NicProvider provider = new NicProvider()) {
+            ProviderContext context = mock(BindingAwareBroker.ProviderContext.class);
+            when(context.getSALService(DataBroker.class)).thenReturn(mock(DataBroker.class));
+            DataBroker broker = mock(DataBroker.class);
+            provider.setDataBroker(broker);
+
+            NicProvider spy = Mockito.spy(provider);
+            Mockito.doNothing().when(spy).initIntentsOperational();
+            Mockito.doNothing().when(spy).initIntentsConfiguration();
+            Mockito.doNothing().when(spy).setDataBroker(broker);
 
             // ensure no exceptions
             // currently this method is empty
-            provider.onSessionInitiated(mock(BindingAwareBroker.ProviderContext.class));
+            spy.onSessionInitiated(context);
         }
     }
 
