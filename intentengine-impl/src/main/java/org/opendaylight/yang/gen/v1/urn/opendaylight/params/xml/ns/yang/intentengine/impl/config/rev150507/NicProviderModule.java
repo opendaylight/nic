593aeb1f73280a7ca333daf5094aab9629ca018e
Raphael Amorim <raphael.amorim@hp.com>
2015-05-08 14:47:25 -0700

    Adding code to initialize the intents config/operational trees
    Change-Id: I587ea367511e617f57e086022adbf4c6a724363a
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

---
+++ intentengine-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/intentengine/impl/config/rev150507/NicProviderModule.java
@@ -0,0 +1,38 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intentengine.impl.config.rev150507;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.intentengine.NicProvider;
+
+public class NicProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intentengine.impl.config.rev150507.AbstractNicProviderModule {
+
+    public NicProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public NicProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+                             org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intentengine.impl.config.rev150507.NicProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        final NicProvider provider = new NicProvider();
+        DataBroker broker = getDataBrokerDependency();
+
+        provider.setDataBroker(broker);
+
+        final class AutoCloseableNicProvider implements AutoCloseable {
+
+            @Override
+            public void close() throws Exception {
+                provider.close();
+            }
+        }
+        return new AutoCloseableNicProvider();
+    }
+}
