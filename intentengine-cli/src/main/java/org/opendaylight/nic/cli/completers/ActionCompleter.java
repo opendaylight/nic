540cdec26672229d25cd17d348c9463db6696c10
Raphael Amorim <raphael.amorim@hp.com>
2015-05-12 10:46:10 -0700

    This introduces intent:add support on intentengine-cli
    This implementation provides a cli command to push new intents to the
    configuration data store. The parameters are:
    
    SYNTAX
            intent:add [options]
    
    OPTIONS
            -a, --actions
                    Action to be performed.
                    -a / --actions BLOCK/ALLOW
                    (defaults to [BLOCK])
            --help
                    Display this help message
            -f, --from
                    First subject.
                    -f / --from] <subject>
                    (defaults to any)
            -t, --to
                    Second Subject.
                    -t / --to <subject>
                    (defaults to any)
    
    Examples: --actions ALLOW --from <subject> --to <subject>
              --actions BLOCK --from <subject>
    
    Change-Id: I36c3be28b49158a401b0736187e30e5f1b236cbe
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

---
+++ intentengine-cli/src/main/java/org/opendaylight/nic/cli/completers/ActionCompleter.java
@@ -0,0 +1,30 @@
+package org.opendaylight.nic.cli.completers;
+
+import java.util.List;
+
+import org.apache.karaf.shell.console.Completer;
+import org.apache.karaf.shell.console.completer.StringsCompleter;
+
+/**
+ * <p>
+ * A very simple completer.
+ * </p>
+ */
+public class ActionCompleter implements Completer {
+
+    /**
+     * @param buffer
+     *            the beginning string typed by the user
+     * @param cursor
+     *            the position of the cursor
+     * @param candidates
+     *            the list of completions proposed to the user
+     */
+    @Override
+    public int complete(String buffer, int cursor, List<String> candidates) {
+        StringsCompleter delegate = new StringsCompleter();
+        delegate.getStrings().add("ALLOW");
+        delegate.getStrings().add("BLOCK");
+        return delegate.complete(buffer, cursor, candidates);
+    }
+}
