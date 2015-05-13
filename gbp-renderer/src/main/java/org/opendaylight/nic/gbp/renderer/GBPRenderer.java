8c07d014a8e3b31dc30b9658b154ecb1d494e7e1
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-05-13 16:31:23 -0400

    (WIP) Add Group based Policy Renderer.
    Change-Id: Ib01fd82e30bef0f6d63e16198780f33f3be0281c
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ gbp-renderer/src/main/java/org/opendaylight/nic/gbp/renderer/GBPRenderer.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015, Inocybe Technologies
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.gbp.renderer;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intent;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GBPRenderer implements DataChangeListener,
+    AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(GBPRenderer.class);
+
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    @Override
+    public void onDataChanged(
+            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+
+        //Pull the list of new items
+        Map<InstanceIdentifier<?>, DataObject> createdObjects = changes.getCreatedData();
+
+        for (Entry<InstanceIdentifier<?>, DataObject> created : createdObjects.entrySet()) {
+            if (created.getValue() != null
+                    && created.getValue() instanceof Intent) {
+
+                //Create a group-based policy and push to GBP processor
+
+            }
+        }
+    }
+
+}
