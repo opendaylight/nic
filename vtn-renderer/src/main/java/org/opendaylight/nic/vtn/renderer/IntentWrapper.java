e2722ad759a1fb11215c641b2038127ff6e697b2
vinoth <m.vinoth@hcl.com>
2015-05-14 04:15:01 +0530

    Add intent implementation using VTN JAVA API
    Change-Id: I2c26598ab5d9835acb15c183eb76b16982f29cc5
    Signed-off-by: vinoth <m.vinoth@hcl.com>

---
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/IntentWrapper.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2015 NEC Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.vtn.renderer;
+
+public class IntentWrapper {
+
+    String entityName;
+    int entityValue;
+    String entityDescription;
+    public String getEntityDescription() {
+        return entityDescription;
+    }
+    public void setEntityDescription(String entityDescription) {
+        this.entityDescription = entityDescription;
+    }
+    public String getEntityName() {
+        return entityName;
+    }
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+    
+    public int getEntityValue() {
+        return entityValue;
+    }
+    public void setEntityValue(int index) {
+        this.entityValue = index;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime
+                * result
+                + ((entityDescription == null) ? 0 : entityDescription
+                        .hashCode());
+        result = prime * result
+                + ((entityName == null) ? 0 : entityName.hashCode());
+        result = prime * result + entityValue;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        IntentWrapper other = (IntentWrapper) obj;
+        if (entityDescription == null) {
+            if (other.entityDescription != null)
+                return false;
+        } else if (!entityDescription.equals(other.entityDescription))
+            return false;
+        if (entityName == null) {
+            if (other.entityName != null)
+                return false;
+        } else if (!entityName.equals(other.entityName))
+            return false;
+        if (entityValue != other.entityValue)
+            return false;
+        return true;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "IntentWrapper [entityName=" + entityName + ", entityValue="
+                + entityValue + ", entityDescription=" + entityDescription
+                + "]";
+    }
+}
--- vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRenderer.java
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRenderer.java
@@ -10,6 +10,11 @@
 package org.opendaylight.nic.vtn.renderer;
 
 import java.util.List;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,12 +29,34 @@ import org.opendaylight.vtn.manager.VTenant;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
+
+import org.opendaylight.controller.sal.utils.ServiceHelper;
+import org.opendaylight.controller.sal.utils.Status;
+import org.opendaylight.vtn.manager.IVTNManager;
+import org.opendaylight.vtn.manager.VBridgeConfig;
+import org.opendaylight.vtn.manager.VBridgePath;
+import org.opendaylight.vtn.manager.VTNException;
+import org.opendaylight.vtn.manager.VTenantConfig;
+import org.opendaylight.vtn.manager.VTenantPath;
+import org.opendaylight.vtn.manager.VlanMapConfig;
+import org.opendaylight.vtn.manager.flow.cond.FlowCondition;
+import org.opendaylight.vtn.manager.flow.cond.FlowMatch;
+import org.opendaylight.vtn.manager.flow.cond.Inet4Match;
+import org.opendaylight.vtn.manager.flow.cond.InetMatch;
+import org.opendaylight.vtn.manager.flow.filter.FlowFilter;
+import org.opendaylight.vtn.manager.flow.filter.FlowFilterId;
+import org.opendaylight.vtn.manager.flow.filter.PassFilter;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.IntentsBuilder;
+
 public class VTNRenderer implements AutoCloseable, DataChangeListener {
     /**
      * Logger instance.
      */
-    private static final Logger  LOG =
-        LoggerFactory.getLogger(VTNRenderer.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(VTNRenderer.class);
+  
 
     /**
      * {@inheritDoc}
@@ -45,31 +72,61 @@ public class VTNRenderer implements AutoCloseable, DataChangeListener {
             AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> ev) {
         // TODO: Change the log level to TRACE.
         LOG.info("Intent configuration changed.");
-
-        IVTNManager mgr = getVTNManager();
+		
+        for (DataObject dao : ev.getCreatedData().values()) {
+			LOG.info("Data change For Loop ENTER:::");
+            if (dao instanceof Intents)
+				LOG.info("FOR LOOP enter :::");
+                try {
+					IntentsBuilder lcl_iB = new IntentsBuilder((Intents)dao);					
+					List<Intent> intent = lcl_iB.getIntent();
+					for(Intent lcl_intent : intent) {
+                        intentParser(lcl_intent);
+					}
+                } catch (Exception e) {
+                    LOG.error("Could not create VTN Renderer", e);
+                }
+        }
 
         /**
-         *  Delete the following lines, and implements the renderer.
+         * Delete the following lines, and implements the renderer.
+         * 
+         * try { List<VTenant> vtns = mgr.getTenants(); for (VTenant vtn: vtns)
+         * { LOG.info("{}", vtn); } } catch (Exception e) {
+         * LOG.info("Failed to get tenants: {}", e); }
          */
-        try {
-            List<VTenant> vtns = mgr.getTenants();
-            for (VTenant vtn: vtns) {
-                LOG.info("{}", vtn);
-            }
-        } catch (Exception e) {
-            LOG.info("Failed to get tenants: {}", e);
-        }
     }
 
     /**
-     * Return the VTN manager service.
-     *
-     * @return  The VTN manager service associated with the specified
-     *          container.
+     * This method parse the intent and calls the VTN renderer
+     * 
+     * @param intents
      */
-    protected IVTNManager getVTNManager() {
-        IVTNManager mgr = (IVTNManager)ServiceHelper.
-            getInstance(IVTNManager.class, "default", this);
-        return mgr;
+    public void intentParser(Intent intent) {
+        String endPointSrc = "";
+        String endPointDst = "";
+        String action = "";
+		HashMap hashMap = new HashMap<String, ArrayList<IntentWrapper>>();
+		ArrayList<IntentWrapper> arrayList = new ArrayList<IntentWrapper>();
+        String intentID = "";
+		VtnIntentParser renderer = new VtnIntentParser();
+        if (intent.getStatus() != null) {
+			
+            if (intent.getSubjects().size() == 2) {
+                endPointSrc = intent.getSubjects().get(0).toString();
+                endPointDst = intent.getSubjects().get(1).toString();
+                if (intent.getActions() != null) {
+                    action = intent.getActions().get(0).toString();
+                    renderer.rendering(endPointSrc, endPointDst, action, arrayList);
+					
+				
+                }
+
+            }
+            hashMap.put(intentID, arrayList);
+		    VTNRendereUtility.storeIntentDetail(hashMap);
+        }
     }
+
+
 }
