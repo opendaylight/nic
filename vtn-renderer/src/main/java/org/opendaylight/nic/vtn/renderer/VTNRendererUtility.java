31faa527e812d6dfb8c7644394e821c61655e833
vinoth <m.vinoth@hcl.com>
2015-05-14 04:15:01 +0530

    Add intent implementation using VTN JAVA API
    Change-Id: I2c26598ab5d9835acb15c183eb76b16982f29cc5
    Signed-off-by: vinoth <m.vinoth@hcl.com>

---
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRendererUtility.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 NEC Corporation
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.vtn.renderer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class VTNRendererUtility {
+    /*
+     * Stores the Intent details as a map
+     */
+    static HashMap<String, ArrayList<IntentWrapper>> hashMapIntentUtil = new HashMap<String, ArrayList<IntentWrapper>>();
+    public static void storeIntentDetail(HashMap hashmapIntent)
+    {
+        hashMapIntentUtil.putAll(hashmapIntent);
+    }
+    
+    
+}
