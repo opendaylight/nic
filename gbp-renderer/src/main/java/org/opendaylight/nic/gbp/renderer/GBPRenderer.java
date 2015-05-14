e567bb453434563162f827f1d14eef94964684b0
Ben Eze <beze@inocybe.ca>
2015-05-14 10:03:28 -0400

    Update copyright headers for GBP Renderer
    Change-Id: Ib3cb7764e07145cf2fa6ef4de15d7f2e9b7989fc
    Signed-off-by: Ben Eze <beze@inocybe.ca>

--- gbp-renderer/src/main/java/org/opendaylight/nic/gbp/renderer/GBPRenderer.java
+++ gbp-renderer/src/main/java/org/opendaylight/nic/gbp/renderer/GBPRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, Inocybe Technologies
+ * Copyright 2015, Inocybe Technologies and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
