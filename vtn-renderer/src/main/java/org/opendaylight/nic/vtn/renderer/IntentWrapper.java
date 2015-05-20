366edcd807c4ded656f23e2ef8f615c425b2d96c
vinoth <m.vinoth@hcl.com>
2015-05-21 01:52:10 +0530

    Clean up the vtn renderer code.
     * Optimized the code, fixed delete issue and addressed internal and external review comments
     * As mentioned above comments applied into  VTNIntenParser, VTNRenderer, IntentWrapper and VTNRendererUtility implementation
    
    Change-Id: Iff4d4b4953edffb0fa307625e4f2580ac831144c
    Signed-off-by: vinoth <m.vinoth@hcl.com>

--- vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/IntentWrapper.java
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/IntentWrapper.java
@@ -6,29 +6,62 @@
  * terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.nic.vtn.renderer;
 
+/**
+ * The IntentWrapper class creates the VTN entity's to be stored.
+ */
 public class IntentWrapper {
 
-    String entityName;
     int entityValue;
+    String entityName;
     String entityDescription;
+
+    /**
+     * Returns the value for the entity.
+     * @return entityDescription  A brief description about the entity type.
+     */
     public String getEntityDescription() {
         return entityDescription;
     }
+
+    /**
+     * Sets the value for the entity.
+     * @param entityDescription  A brief description about the entity type.
+     */
     public void setEntityDescription(String entityDescription) {
         this.entityDescription = entityDescription;
     }
+
+    /**
+     * Returns the entity type that is stored.
+     * @return entityName  Name of the entity stored.
+     */
     public String getEntityName() {
         return entityName;
     }
+
+    /**
+     * The entity type to be stored.
+     * @param entityName  Type of the entity stored.
+     */
     public void setEntityName(String entityName) {
         this.entityName = entityName;
     }
 
+    /**
+     * Retrieves the index value of entity.
+     * @return entityValue  Entity id stored.
+     */
     public int getEntityValue() {
         return entityValue;
     }
+
+    /**
+     * Sets the index value of entity.
+     * @param entityValue  index value of the entity.
+     */
     public void setEntityValue(int index) {
         this.entityValue = index;
     }
@@ -46,6 +79,7 @@ public class IntentWrapper {
         result = prime * result + entityValue;
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
