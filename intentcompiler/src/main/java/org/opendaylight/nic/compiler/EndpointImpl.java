ad9773662547e2029f77f4255b13d2527cc2f51c
Renato Aguiar <renato.aguiar@hp.com>
2015-05-19 21:52:05 -0700

    DRAFT: Add intentcompiler service
    Change-Id: I86c2ad1e42167aa607dffcd47825e2df4a748caf
    Signed-off-by: Renato Aguiar <renato.aguiar@hp.com>

---
+++ intentcompiler/src/main/java/org/opendaylight/nic/compiler/EndpointImpl.java
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this distribution,
+// and is available at http://www.eclipse.org/legal/epl-v10.html
+//------------------------------------------------------------------------------
+
+package org.opendaylight.nic.compiler;
+
+import org.opendaylight.nic.compiler.api.Endpoint;
+
+import java.net.InetAddress;
+
+public class EndpointImpl implements Endpoint {
+    InetAddress ipAddress;
+
+    public EndpointImpl(InetAddress ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EndpointImpl endpoint = (EndpointImpl) o;
+
+        return !(ipAddress != null ? !ipAddress.equals(endpoint.ipAddress) : endpoint.ipAddress != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return ipAddress != null ? ipAddress.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return ipAddress.getHostAddress();
+    }
+}
---
+++ intentcompiler/src/main/java/org/opendaylight/nic/compiler/IntentCompilerImpl.java
@@ -0,0 +1,113 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this distribution,
+// and is available at http://www.eclipse.org/legal/epl-v10.html
+//------------------------------------------------------------------------------
+package org.opendaylight.nic.compiler;
+
+import com.google.common.collect.Sets;
+import org.opendaylight.nic.compiler.api.Action;
+import org.opendaylight.nic.compiler.api.Endpoint;
+import org.opendaylight.nic.compiler.api.IntentCompiler;
+import org.opendaylight.nic.compiler.api.Policy;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
+
+public class IntentCompilerImpl implements IntentCompiler {
+    @Override
+    public Collection<Policy> compile(Collection<Policy> policies) {
+        Queue<Policy> conflictingPolicies = new LinkedList<>(policies);
+        Collection<Policy> compiledPolicies = new LinkedList<>();
+        while (!conflictingPolicies.isEmpty()) {
+            Policy policy = conflictingPolicies.remove();
+            Iterator<Policy> iterator2 = conflictingPolicies.iterator();
+            Collection<Policy> results = new LinkedList<>();
+            while (iterator2.hasNext()) {
+                Policy policy2 = iterator2.next();
+                if (conflicts(policy, policy2)) {
+                    iterator2.remove();
+                    results.addAll(transform(policy, policy2));
+                }
+            }
+            if (results.isEmpty()) {
+                compiledPolicies.add(policy);
+            } else {
+                conflictingPolicies.addAll(results);
+            }
+        }
+        return compiledPolicies;
+    }
+
+    @Override
+    public Set<Endpoint> parseEndpointGroup(String csv) throws UnknownHostException {
+        Set<Endpoint> endpoints = new LinkedHashSet<>();
+        String[] ipAdresses = csv.split(",");
+        for (String ipAddress : ipAdresses) {
+            endpoints.add(new EndpointImpl(InetAddress.getByName(ipAddress)));
+        }
+        return endpoints;
+    }
+
+    @Override
+    public Policy createPolicy(Set<Endpoint> source, Set<Endpoint> destination, Action action) {
+        return new PolicyImpl(source, destination, action);
+    }
+
+    private Collection<Policy> transform(Policy p1, Policy p2) {
+        Collection<Policy> policies = new LinkedList<>();
+        Sets.SetView<Endpoint> src, dst;
+
+        src = Sets.difference(p1.src(), p2.src());
+        if (!src.isEmpty()) {
+            dst = Sets.difference(p1.dst(), p2.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, p1.action()));
+            }
+            dst = Sets.intersection(p1.dst(), p2.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, p1.action()));
+            }
+        }
+        src = Sets.intersection(p1.src(), p2.src());
+        if (!src.isEmpty()) {
+            dst = Sets.intersection(p1.dst(), p2.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, merge(p1.action(), p2.action())));
+            }
+            dst = Sets.difference(p2.dst(), p1.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, p2.action()));
+            }
+        }
+        src = Sets.difference(p2.src(), p1.src());
+        if (!src.isEmpty()) {
+            dst = Sets.intersection(p1.dst(), p2.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, p2.action()));
+            }
+            dst = Sets.difference(p2.dst(), p1.dst());
+            if (!dst.isEmpty()) {
+                policies.add(new PolicyImpl(src, dst, p2.action()));
+            }
+        }
+
+        return policies;
+    }
+
+    private Action merge(Action a1, Action a2) {
+        if (Action.BLOCK.equals(a1) || Action.BLOCK.equals(a2))
+            return Action.BLOCK;
+        return Action.ALLOW;
+    }
+
+    private boolean conflicts(Policy p1, Policy p2) {
+        if (!Sets.intersection(p1.src(), p2.src()).isEmpty()
+                && !Sets.intersection(p1.dst(), p2.dst()).isEmpty())
+            return true;
+        return false;
+    }
+}
