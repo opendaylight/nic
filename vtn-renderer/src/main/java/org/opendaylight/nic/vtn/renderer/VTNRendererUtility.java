4d44b915b3164f08c5cc1e8669063ac95658f2b5
vinoth <m.vinoth@hcl.com>
2015-05-21 01:52:10 +0530

    Review comments addressed.
     * Optimized the code, fixed delete issue and addressed internal and external review comments
     * As mentioned above comments applied into  VTNIntenParser, VTNRenderer, IntentWrapper and VTNRendererUtility implementation
    
    Change-Id: Iff4d4b4953edffb0fa307625e4f2580ac831144c
    Signed-off-by: vinoth <m.vinoth@hcl.com>

--- vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRendererUtility.java
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRendererUtility.java
@@ -6,19 +6,108 @@
  * terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.nic.vtn.renderer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The VTNRendererUtility consists of common methods to perform general functions.
+ */
 public class VTNRendererUtility {
-    /*
-     * Stores the Intent details as a map
-     */
-    static HashMap<String, ArrayList<IntentWrapper>> hashMapIntentUtil = new HashMap<String, ArrayList<IntentWrapper>>();
 
-    public static void storeIntentDetail(HashMap hashmapIntent)
+    static Map<String, List<IntentWrapper>> hashMapIntentUtil = new HashMap<String, List<IntentWrapper>>();
+
+    private final Logger LOG = LoggerFactory.getLogger(VTNRendererUtility.class);
+
+    /**
+     * Stores the Intent details as a map
+     * @param hashmapIntent
+     */
+    public static void storeIntentDetail(Map hashmapIntent)
     {
         hashMapIntentUtil.putAll(hashmapIntent);
+    }
+
+    /**
+     * Validates the received IP address
+     *
+     * @param ip
+     * @return {@code = true} on valid IP address.
+     */
+    public boolean validateIP(final String ip) {
+        if (ip == null) {
+            LOG.error("IP address is null");
+            throw new NullPointerException();
+        }
+
+        String ipAddressPattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+        Pattern pattern = Pattern.compile(ipAddressPattern);
+        Matcher matcher = pattern.matcher(ip);
+        return matcher.matches();
+    }
+
+    /**
+     * Validate the given MAC address
+     *
+     * @param macAddress
+     * @return {@code = true} on valid MAC address
+     */
+    public boolean validateMacAddress(final String macAddress) {
+        if (macAddress == null) {
+            LOG.error("MAC address is null");
+            throw new NullPointerException();
+        }
+
+        String macAdrressPattern = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
+        Pattern pattern = Pattern.compile(macAdrressPattern);
+        Matcher matcher = pattern.matcher(macAddress);
+        return matcher.matches();
+    }
+
+    /**
+     * This method verifies if both the ip exists on the same subnet
+     *
+     * @param srcIp
+     * @param dstIp
+     * @return  {@code = false} the given IP addresses are not in same subnet.
+     */
+    public boolean validateSubnet(String srcIp, String dstIp) {
+        if (srcIp == null || dstIp == null) {
+            LOG.error("Source or Destination IP address is null");
+            throw new NullPointerException();
+        }
+
+        if (!srcIp.equalsIgnoreCase(dstIp)) {
+            String[] srcIpDigits = srcIp.split("\\.");
+            String[] dstIpDigits = dstIp.split("\\.");
+
+            for (int index = 0; index < 3; index++) {
+                if (!(Byte.parseByte(srcIpDigits[index]) == Byte
+                        .parseByte(dstIpDigits[index]))) {
+                    LOG.trace(
+                            "Source and Destination IP addresses are not in same subnet {} - {}",
+                            srcIp, dstIp);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        LOG.trace(
+                "Source and Destination IP addresses have same IP addresses {} - {}",
+                srcIp, dstIp);
+        return false;
     }
 }
