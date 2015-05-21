a00fb1d97ac38639a53df6344aff8b43524f42b7
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-05-21 11:50:40 -0400

    Added scaffolding for GBP Renderer change listener
    Change-Id: I3d28f30ff7fdaa30aacca21cdbf3c0294a4875ab
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

--- gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/nic/gbp/renderer/impl/GBPRenderer.java
+++ gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/nic/gbp/renderer/impl/GBPRenderer.java
@@ -8,42 +8,104 @@
 
 package org.opendaylight.nic.gbp.renderer.impl;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intent;
-import org.opendaylight.yangtools.yang.binding.DataObject;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.IntentsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.IntentBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GBPRenderer implements DataChangeListener,
-    AutoCloseable {
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+
+public class GBPRenderer implements AutoCloseable, BindingAwareProvider {
 
     private static final Logger LOG = LoggerFactory
             .getLogger(GBPRenderer.class);
+    private DataBroker dataBroker;
+    private final ExecutorService executor;
 
-
-    @Override
-    public void close() throws Exception {
-
+    public GBPRenderer(DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+        executor = Executors.newFixedThreadPool(1);
     }
 
     @Override
-    public void onDataChanged(
-            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("GBP Renderer Provider Session Initiated");
+        dataBroker = session.getSALService(DataBroker.class);
+        initializeGbpRendererTopology(LogicalDatastoreType.OPERATIONAL);
+        initializeGbpRendererTopology(LogicalDatastoreType.CONFIGURATION);
+    }
 
-        //Pull the list of new items
-        Map<InstanceIdentifier<?>, DataObject> createdObjects = changes.getCreatedData();
+    @Override
+    public void close() throws Exception {
+        LOG.info("GBPRenderer Closed");
+        if (dataBroker != null) {
+            deleteNode(GBPRendererConstants.INTENTS_IID);
+            deleteNode(GBPRendererConstants.INTENT_IID);
+        }
+        executor.shutdown();
+    }
 
-        for (Entry<InstanceIdentifier<?>, DataObject> created : createdObjects.entrySet()) {
-            if (created.getValue() != null
-                    && created.getValue() instanceof Intent) {
-
-                //Create a group-based policy and push to GBP processor
-
+    private void deleteNode(InstanceIdentifier<?> iid) {
+        WriteTransaction writeTransactionNodes = dataBroker.newWriteOnlyTransaction();
+        writeTransactionNodes.delete(LogicalDatastoreType.OPERATIONAL, iid);
+        Futures.addCallback(writeTransactionNodes.submit(), new FutureCallback<Void>() {
+            @Override
+            public void onSuccess(Void result) {
+                LOG.debug("Delete result: " + result);
             }
+
+            @Override
+            public void onFailure(Throwable t) {
+                LOG.error("Delete failed", t);
+            }
+        });
+    }
+
+    private void initializeGbpRendererTopology(LogicalDatastoreType type) {
+        ReadWriteTransaction transaction = dataBroker.newReadWriteTransaction();
+        initializeIntentTopology(transaction, type);
+        CheckedFuture<Optional<Intent>, ReadFailedException> intentTp = transaction
+                .read(type, GBPRendererConstants.INTENT_IID);
+        try {
+            if (!intentTp.get().isPresent()) {
+                IntentBuilder ib = new IntentBuilder();
+                ib.setIntentId(GBPRendererConstants.GBP_RENDERER_INTENT_ID);
+                transaction.put(type, GBPRendererConstants.INTENT_IID, ib.build());
+                transaction.submit();
+            } else {
+                transaction.cancel();
+            }
+        } catch (Exception e) {
+            LOG.error("Error initializing intent topology {}", e);
+        }
+    }
+
+    private void initializeIntentTopology(ReadWriteTransaction transaction, LogicalDatastoreType type) {
+        CheckedFuture<Optional<Intents>, ReadFailedException> intentsTp = transaction
+                .read(type,GBPRendererConstants.INTENTS_IID);
+        try {
+            if (!intentsTp.get().isPresent()) {
+                IntentsBuilder isb = new IntentsBuilder();
+                transaction.put(type, GBPRendererConstants.INTENTS_IID, isb.build());
+            }
+        } catch (Exception e) {
+            LOG.error("Error initializing intent topology {}", e);
         }
     }
 
--- gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/gbp/renderer/impl/rev150511/GBPRendererModule.java
+++ gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/gbp/renderer/impl/rev150511/GBPRendererModule.java
@@ -1,18 +1,13 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.gbp.renderer.impl.rev150511;
 
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.nic.gbp.renderer.impl.GBPRenderer;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.nic.gbp.renderer.impl.GBPRendererDataChangeListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GBPRendererModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.nic.gbp.renderer.impl.rev150511.AbstractGBPRendererModule {
 
+    private GBPRendererDataChangeListener gBPRendererDataChangeListener;
     private static final Logger LOG = LoggerFactory.getLogger(GBPRendererModule.class);
 
     public GBPRendererModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
@@ -30,31 +25,21 @@ public class GBPRendererModule extends org.opendaylight.yang.gen.v1.urn.opendayl
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        DataBroker broker = getDataBrokerDependency();
-
-        LogicalDatastoreType store = LogicalDatastoreType.CONFIGURATION;
-        InstanceIdentifier<Intents> path = InstanceIdentifier.builder(Intents.class).build();
-
-        final GBPRenderer renderer = new GBPRenderer();
-        DataChangeScope scope = DataChangeScope.SUBTREE;
-        final ListenerRegistration<DataChangeListener> reg = broker.registerDataChangeListener(store, path, renderer, scope);
-
+        final GBPRenderer renderer = new GBPRenderer(getDataBrokerDependency());
+        gBPRendererDataChangeListener = new GBPRendererDataChangeListener(getDataBrokerDependency());
         final class CloseResources implements AutoCloseable {
             @Override
             public void close() throws Exception {
                 if (renderer != null) {
                     renderer.close();
                 }
-                if (reg != null) {
-                    reg.close();
+                if (gBPRendererDataChangeListener != null) {
+                    gBPRendererDataChangeListener.close();
                 }
-                LOG.info("GBG Renderer (instance {}) torn down.", this);
+                LOG.info("GBP Renderer (instance {}) torn down.", this);
             }
         }
-
-        AutoCloseable ret = new CloseResources();
-        LOG.info("GBG Renderer: (instance {}) initialized.", ret);
-        return ret;
+        return renderer;
     }
 
 }
