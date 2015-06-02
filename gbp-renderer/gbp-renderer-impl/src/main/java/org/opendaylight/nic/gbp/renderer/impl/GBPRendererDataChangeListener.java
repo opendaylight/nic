d88de3e6cb82a8c000c90600ddd11ac463d09758
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-06-02 14:37:38 -0400

    Refactor and rework of GBPtenantPolicyCreator and GPBRendererDataChangeListener.
    Change-Id: Iafc4ff0bf60fefdd77878534aede9d4d89c8a5a4
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/nic/gbp/renderer/impl/GBPRendererDataChangeListener.java
+++ gbp-renderer/gbp-renderer-impl/src/main/java/org/opendaylight/nic/gbp/renderer/impl/GBPRendererDataChangeListener.java
@@ -8,61 +8,31 @@
 
 package org.opendaylight.nic.gbp.renderer.impl;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.gbp.renderer.rev150511.GbpRendererAugmentation;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.gbp.renderer.rev150511.GbpRendererAugmentationBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.common.rev140421.EndpointGroupId;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.common.rev140421.TargetName;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.common.rev140421.UniqueId;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.Tenants;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.Tenant;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.Contract;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.ContractBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.EndpointGroup;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.EndpointGroupKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.contract.Target;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.policy.rev140421.tenants.tenant.contract.TargetBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Actions;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Conditions;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Constraints;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Subjects;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.end.point.selector.EndPointSelector;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.end.point.group.selector.EndPointGroupSelector;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.end.point.group.EndPointGroup;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.IntentBuilder;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.Augmentation;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
-
 public class GBPRendererDataChangeListener implements DataChangeListener,
         AutoCloseable {
 
-    private static final Logger LOG = LoggerFactory
-            .getLogger(GBPRendererDataChangeListener.class);
-    @SuppressWarnings("unused")
+    private static final Logger LOG = LoggerFactory.getLogger(GBPRendererDataChangeListener.class);
+
     private DataBroker dataBroker;
     private ListenerRegistration<DataChangeListener> gbpRendererListener = null;
-    private final ReadWriteTransaction transaction;
 
     public GBPRendererDataChangeListener(DataBroker dataBroker) {
         this.dataBroker = dataBroker;
-        this.transaction = dataBroker.newReadWriteTransaction();
         gbpRendererListener = dataBroker.registerDataChangeListener(
                 LogicalDatastoreType.CONFIGURATION,
                 GBPRendererConstants.INTENTS_IID, this, DataChangeScope.SUBTREE);
@@ -71,56 +41,36 @@ public class GBPRendererDataChangeListener implements DataChangeListener,
     @Override
     public void onDataChanged(
             AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
-        create(changes);
-        update(changes);
+        create(changes.getCreatedData());
+        update(changes.getUpdatedData());
         delete(changes);
+    }
+
+    private void create(Map<InstanceIdentifier<?>, DataObject> changes) {
+
+        for (Entry<InstanceIdentifier<?>, DataObject> created : changes.entrySet()) {
+
+            if (created.getValue() != null && created.getValue() instanceof Intent) {
+
+                Intent intent = (Intent) created.getValue();
+
+                LOG.info("New intent created with id {}.", intent);
+
+                //This may generate conflicts since actions tend to oppose each other
+                GBPTenantPolicyCreator createGBPolicy = new GBPTenantPolicyCreator(this.dataBroker, intent);
+
+                createGBPolicy.processIntentToGBP();
+            }
+        }
+    }
+
+    private void update(Map<InstanceIdentifier<?>, DataObject> changes) {
+        // TODO implement update
     }
 
     private void delete(
             AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
-        for (Entry<InstanceIdentifier<?>, DataObject> updated : changes
-                .getUpdatedData().entrySet()) {
-            LOG.info("Delete requested on intent id {}", updated.getKey());
-            // TODO implement delete, verify old data versus new data
-        }
-    }
-
-    private void update(
-            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
-        for (Entry<InstanceIdentifier<?>, DataObject> updated : changes
-                .getUpdatedData().entrySet()) {
-            LOG.info("Intent {} has been modified.", updated.getKey());
-            // TODO implement update
-        }
-    }
-
-    private void create(
-            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
-        for (Entry<InstanceIdentifier<?>, DataObject> created : changes
-                .getCreatedData().entrySet()) {
-            LOG.info("New intent added with id {}.", created.getKey());
-            if (created.getValue() != null) {
-                if (created instanceof Intent) {
-                    Intent intent = (Intent)created.getValue();
-
-                    //Pull the components of the intent and activate the process to process the intent
-                    List<Subjects> subjects = intent.getSubjects();
-                    List<Actions> actions = intent.getActions();
-                    List<Conditions> conditions = intent.getConditions();
-                    List<Constraints> constraints = intent.getConstraints();
-
-                    //This may generate conflicts since actions tend to oppose each other
-                    for (Actions action: actions) {
-                        GBPTenantPolicyCreator createGBPolicy = new GBPTenantPolicyCreator(this.dataBroker,
-                                subjects,
-                                action, conditions,
-                                constraints);
-
-                        createGBPolicy.processIntentToGBP();
-                    }
-                }
-            }
-        }
+        // TODO implement delete, verify old data versus new data
     }
 
     @Override
