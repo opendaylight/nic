78ca12c500058e50a15375a36b6ce792f988c76d
Himal Kumar <himal.kumar@hp.com>
2015-06-09 14:58:55 -0700

    Adding support for Classifiers in the policy compiler.
    Change-Id: I1930766c924d243792aa9c2fd878c3ed17c2e18e
    Signed-off-by: Himal Kumar <himal.kumar@hp.com>

--- intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/L4DstTermType.java
+++ intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/L4DstTermType.java
@@ -1,22 +1,26 @@
 //------------------------------------------------------------------------------
 // Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
-//
+// 
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v1.0 which accompanies this distribution,
 // and is available at http://www.eclipse.org/legal/epl-v10.html
 //------------------------------------------------------------------------------
-package org.opendaylight.nic.compiler.api;
+package org.opendaylight.nic.compiler;
 
-import java.util.Set;
+import org.opendaylight.nic.compiler.api.TermLabel;
 
-import org.opendaylight.nic.compiler.ClassifierImpl;
+public class L4DstTermType extends TermTypeBase {
 
-public interface Policy {
-    Set<Endpoint> src();
+    private static final int L4_DST_MIN = 0;
+    private static final int L4_DST_MAX = 65535;
+    private static final L4DstTermType INSTANCE = new L4DstTermType();
 
-    Set<Endpoint> dst();
+    public static L4DstTermType getInstance() {
+        return INSTANCE;
+    }
 
-    Set<Action> action();
+    private L4DstTermType() {
+        super(new TermLabel("L4_DST"), L4_DST_MIN, L4_DST_MAX);
+    }
 
-    ClassifierImpl classifier();
 }
