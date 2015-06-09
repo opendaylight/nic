071c71fbcb89f7e410e670472d7a9876f136e2d8
Himal Kumar <himal.kumar@hp.com>
2015-06-09 14:58:55 -0700

    DO NOT MERGE : Adding Classifiers
    Change-Id: I1930766c924d243792aa9c2fd878c3ed17c2e18e
    Signed-off-by: Himal Kumar <himal.kumar@hp.com>

---
+++ intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/TermTypeBase.java
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this distribution,
+// and is available at http://www.eclipse.org/legal/epl-v10.html
+//------------------------------------------------------------------------------
+package org.opendaylight.nic.compiler;
+
+import org.opendaylight.nic.compiler.api.Interval;
+import org.opendaylight.nic.compiler.api.TermLabel;
+import org.opendaylight.nic.compiler.api.TermType;
+
+/**
+ * Base class for creating {@link TermType}s which are registered for use in
+ * {@link Classifier}s.
+ * 
+ */
+public abstract class TermTypeBase implements TermType {
+
+    private final TermLabel label;
+    private final int min;
+    private final int max;
+
+    @Override
+    public int min() {
+        return min;
+    }
+
+    @Override
+    public int max() {
+        return max;
+    }
+
+    public TermTypeBase(TermLabel label, int min, int max) {
+        super();
+        this.label = new TermLabel(label.toString());
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public TermLabel label() {
+        return new TermLabel(label.toString());
+    }
+
+    @Override
+    public boolean isLegal(Interval interval) {
+        return ((interval.start() >= min()) && (interval.end() <= max()));
+    }
+
+    @Override
+    public boolean isMax(Interval interval) {
+        return ((interval.start() == min()) && (interval.end() == max()));
+    }
+
+    @Override
+    public String toString() {
+        return "TermType { label=" + label + "}";
+    }
+
+}
---
+++ intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/api/Expression.java
@@ -0,0 +1,57 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this distribution,
+// and is available at http://www.eclipse.org/legal/epl-v10.html
+//------------------------------------------------------------------------------
+package org.opendaylight.nic.compiler.api;
+
+import java.util.Collection;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/*
+ * An expression within a classifier. The expression is a collection of terms,
+ * ALL of which must match the traffic for the expression to be considered a
+ * match.
+ *
+ * More specifically, the Terms can be thought of as logically AND'd together to
+ * form the Expressions.
+ *
+ */
+public interface Expression {
+
+    /**
+     * Returns the set of term labels in this expression.
+     * 
+     * @return set of term labels
+     */
+    public Set<? extends TermLabel> getTermTypeLabels();
+
+    /**
+     * Returns the set of terms in this expression. A term includes both the
+     * term label and term interval.
+     * 
+     * @return set of terms
+     */
+    public Collection<? extends Term> getTerms();
+
+    /**
+     * Returns the set of entries in this expression, where each entry is a
+     * unique term label along with the term to which it applies.
+     * 
+     * @return set of entries
+     */
+    public Set<? extends Entry<? extends TermLabel, ? extends Term>> getEntries();
+
+    /**
+     * Returns the term associated with the given term label.
+     * 
+     * @param label
+     *            term label
+     * @return associated term
+     */
+    public Term getTerm(TermLabel label);
+
+}
