94f0fb42b6233de2f0ff6344ef1b3f8298581d87
Himal Kumar <himal.kumar@hp.com>
2015-06-09 14:58:55 -0700

    DO NOT MERGE : Adding Classifiers
    Change-Id: I1930766c924d243792aa9c2fd878c3ed17c2e18e
    Signed-off-by: Himal Kumar <himal.kumar@hp.com>

--- intentengine-compiler/src/test/java/org/opendaylight/nic/compiler/IntentCompilerImplTest.java
+++ intentengine-compiler/src/test/java/org/opendaylight/nic/compiler/IntentCompilerImplTest.java
@@ -7,13 +7,9 @@
 //------------------------------------------------------------------------------
 package org.opendaylight.nic.compiler;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.opendaylight.nic.compiler.api.Action;
-import org.opendaylight.nic.compiler.api.ActionConflictType;
-import org.opendaylight.nic.compiler.api.BasicAction;
-import org.opendaylight.nic.compiler.api.Endpoint;
-import org.opendaylight.nic.compiler.api.Policy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -23,13 +19,20 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.nic.compiler.api.Action;
+import org.opendaylight.nic.compiler.api.ActionConflictType;
+import org.opendaylight.nic.compiler.api.BasicAction;
+import org.opendaylight.nic.compiler.api.Endpoint;
+import org.opendaylight.nic.compiler.api.Policy;
 
 public class IntentCompilerImplTest {
     IntentCompilerImpl intentCompiler;
     Action allow, block, redirect, monitor;
 
-    private Set<Endpoint> endpoints(String... hosts) throws UnknownHostException {
+    private Set<Endpoint> endpoints(String... hosts)
+            throws UnknownHostException {
         Set<Endpoint> endpoints = new LinkedHashSet<>();
         for (String host : hosts) {
             Endpoint endpoint = new EndpointImpl(InetAddress.getByName(host));
@@ -46,7 +49,8 @@ public class IntentCompilerImplTest {
         return actionSet;
     }
 
-    private void testCompile(Collection<Policy> input, Collection<Policy> output) throws Exception {
+    private void testCompile(Collection<Policy> input, Collection<Policy> output)
+            throws Exception {
         Collection<Policy> compiledPolicies = intentCompiler.compile(input);
         assertNotNull(compiledPolicies);
         assertEquals(output.size(), compiledPolicies.size());
@@ -64,97 +68,167 @@ public class IntentCompilerImplTest {
 
     @Test
     public void testEmptyCompile() throws Exception {
-        testCompile(Collections.<Policy>emptyList(), Collections.<Policy>emptyList());
+        testCompile(Collections.<Policy> emptyList(),
+                Collections.<Policy> emptyList());
     }
 
     @Test
     public void testEmptyEndpointsCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints(), endpoints(), actions(allow))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints(), endpoints(), actions(allow))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(endpoints(),
+                endpoints(), actions(allow))), Arrays.asList(intentCompiler
+                .createPolicy(endpoints(), endpoints(), actions(allow))));
     }
 
     @Test
     public void testNonConflictCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.3"), endpoints("10.0.0.4"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.3"), endpoints("10.0.0.4"), actions(block))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow)),
+                intentCompiler.createPolicy(endpoints("10.0.0.3"),
+                        endpoints("10.0.0.4"), actions(block))), Arrays.asList(
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.2"), actions(allow)), intentCompiler
+                        .createPolicy(endpoints("10.0.0.3"),
+                                endpoints("10.0.0.4"), actions(block))));
+
     }
 
     @Test
     public void testConflictCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow)),
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.2"), actions(block))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.2"),
+                        actions(block))));
     }
 
     @Test
     public void testConflictMergeCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1", "10.0.0.2"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.2"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1", "10.0.0.2"), endpoints("10.0.0.3"),
+                actions(allow)), intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.2"), endpoints("10.0.0.3"),
+                        actions(allow)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.3"),
+                        actions(block))));
     }
 
     @Test
     public void testConflictThreeCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1", "10.0.0.2"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block)),
-                intentCompiler.createPolicy(endpoints("10.0.0.2"), endpoints("10.0.0.3"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block)),
-                intentCompiler.createPolicy(endpoints("10.0.0.2"), endpoints("10.0.0.3"), actions(block))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1", "10.0.0.2"), endpoints("10.0.0.3"),
+                actions(allow)), intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block)),
+                intentCompiler.createPolicy(endpoints("10.0.0.2"),
+                        endpoints("10.0.0.3"), actions(block))), Arrays.asList(
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.3"), actions(block)), intentCompiler
+                        .createPolicy(endpoints("10.0.0.2"),
+                                endpoints("10.0.0.3"), actions(block))));
     }
 
     @Test
     public void testConflictDestinationCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2", "10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2", "10.0.0.3"),
+                actions(allow)), intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.3"),
+                        actions(allow)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.2"),
+                        actions(block))));
     }
 
     @Test
     public void testConflictDestination2Compile() throws Exception {
         testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2", "10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2", "10.0.0.5"), actions(block))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.5"), actions(block))
-        ));
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.2", "10.0.0.3"), actions(allow)),
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.2", "10.0.0.5"), actions(block))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.3"),
+                        actions(allow)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.2"),
+                        actions(block)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.5"),
+                        actions(block))));
     }
 
     @Test
     public void testConflictMergeActionsCompile() throws Exception {
-        testCompile(Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1", "10.0.0.2", "10.0.0.10"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3", "10.0.0.20"), actions(block)),
-                intentCompiler.createPolicy(endpoints("10.0.0.2"), endpoints("10.0.0.3"), actions(redirect)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.20"), actions(monitor))
-        ), Arrays.asList(
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"), actions(block)),
-                intentCompiler.createPolicy(endpoints("10.0.0.10"), endpoints("10.0.0.3"), actions(allow)),
-                intentCompiler.createPolicy(endpoints("10.0.0.2"), endpoints("10.0.0.3"), actions(allow, redirect)),
-                intentCompiler.createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.20"), actions(block, monitor))
-        ));
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1", "10.0.0.2", "10.0.0.10"),
+                endpoints("10.0.0.3"), actions(allow)), intentCompiler
+                .createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.3", "10.0.0.20"), actions(block)),
+                intentCompiler.createPolicy(endpoints("10.0.0.2"),
+                        endpoints("10.0.0.3"), actions(redirect)),
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.20"), actions(monitor))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.3"),
+                        actions(block)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.10"), endpoints("10.0.0.3"),
+                        actions(allow)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.2"), endpoints("10.0.0.3"),
+                        actions(allow, redirect)), intentCompiler.createPolicy(
+                        endpoints("10.0.0.1"), endpoints("10.0.0.20"),
+                        actions(block, monitor))));
     }
+
+    @Test
+    public void testClassifierNonConflicting() throws Exception {
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow),
+                ClassifierHelper.vlan(10, 20)), intentCompiler.createPolicy(
+                endpoints("10.0.0.3"), endpoints("10.0.0.4"), actions(block),
+                ClassifierHelper.vlan(10, 40))), Arrays.asList(intentCompiler
+                .createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"),
+                        actions(allow), ClassifierHelper.vlan(10, 20)),
+                intentCompiler.createPolicy(endpoints("10.0.0.3"),
+                        endpoints("10.0.0.4"), actions(block),
+                        ClassifierHelper.vlan(10, 40))));
+
+    }
+
+    @Test
+    public void testClassifierConflictCompile() throws Exception {
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(allow),
+                ClassifierHelper.vlan(10, 20)), intentCompiler.createPolicy(
+                endpoints("10.0.0.1"), endpoints("10.0.0.2"), actions(block),
+                ClassifierHelper.vlan(30, 40))), Arrays.asList(intentCompiler
+                .createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.2"),
+                        actions(allow), ClassifierHelper.vlan(10, 20)),
+                intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                        endpoints("10.0.0.2"), actions(block),
+                        ClassifierHelper.vlan(30, 40))));
+    }
+
+    @Test
+    public void testClassifier2Compile() throws Exception {
+        testCompile(Arrays.asList(intentCompiler.createPolicy(
+                endpoints("10.0.0.1", "10.0.0.2"), endpoints("10.0.0.3"),
+                actions(allow), ClassifierHelper.vlan(5, 20)), intentCompiler
+                .createPolicy(endpoints("10.0.0.1"), endpoints("10.0.0.3"),
+                        actions(block), ClassifierHelper.vlan(5, 10))),
+                Arrays.asList(intentCompiler.createPolicy(
+                        endpoints("10.0.0.2"), endpoints("10.0.0.3"),
+                        actions(allow), ClassifierHelper.vlan(11, 20)),
+                        intentCompiler.createPolicy(endpoints("10.0.0.1"),
+                                endpoints("10.0.0.3"), actions(block),
+                                ClassifierHelper.vlan(5, 10)), intentCompiler
+                                .createPolicy(endpoints("10.0.0.1"),
+                                        endpoints("10.0.0.3"), actions(allow),
+                                        ClassifierHelper.vlan(11, 20)),
+                        intentCompiler.createPolicy(endpoints("10.0.0.2"),
+                                endpoints("10.0.0.3"), actions(allow),
+                                ClassifierHelper.vlan(5, 10))));
+    }
+
 }
