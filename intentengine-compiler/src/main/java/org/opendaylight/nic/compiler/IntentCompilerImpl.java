ef54af9f999f83c54b4202ae7fb5781b96be28f7
Himal Kumar <himal.kumar@hp.com>
2015-07-07 10:52:46 -0700

    demo
    Change-Id: I264507f2a038a2ca2b4f714c30fda08e4d44efa2
    Signed-off-by: Himal Kumar <himal.kumar@hp.com>

--- intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/IntentCompilerImpl.java
+++ intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/IntentCompilerImpl.java
@@ -7,8 +7,6 @@
 //------------------------------------------------------------------------------
 package org.opendaylight.nic.compiler;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -17,21 +15,56 @@ import java.util.Queue;
 import java.util.Set;
 
 import org.opendaylight.nic.compiler.api.Action;
-import org.opendaylight.nic.compiler.api.Endpoint;
 import org.opendaylight.nic.compiler.api.IntentCompiler;
-import org.opendaylight.nic.compiler.api.IntentCompilerException;
 import org.opendaylight.nic.compiler.api.Policy;
 
 import com.google.common.collect.Sets;
 
+import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+import edu.uci.ics.jung.graph.util.Pair;
+
+// The class for maintaining the graph of policies : It accepts src EPG, dst EPG and
+// associated actions and puts them in the graph.
+
 public class IntentCompilerImpl implements IntentCompiler {
 
-    private final Transform transform;
+    private static DirectedGraph<Epg, Edge> policyGraph =
+            new DirectedSparseGraph<Epg, Edge>();
+
+    public void addPolicy(Policy policy) {
+
+        if (!policyGraph.containsVertex(policy.src())) {
+            policyGraph.addVertex(policy.src());
+        }
+        if (!policyGraph.containsVertex(policy.dst())) {
+            policyGraph.addVertex(policy.dst());
+        }
+
+        Edge edge = new Edge(policy.action());
+        policyGraph
+                .addEdge(edge, policy.src(), policy.dst(), EdgeType.DIRECTED);
+
+    }
+
+    public void printPolicyGraph() {
+
+        Collection<Edge> edges = policyGraph.getEdges();
+
+        for (Edge edge : edges) {
+
+            Pair<Epg> epgPair = policyGraph.getEndpoints(edge);
+            System.out.println("Source : " + epgPair.getFirst().toString()
+                    + "  Destination : " + epgPair.getSecond().toString()
+                    + "  Acion : " + edge.getAction());
+        }
+    }
 
     @Override
-    public Collection<Policy> compile(Collection<Policy> policies) throws IntentCompilerException {
+    public DirectedGraph<Epg, Edge> compile(Collection<Policy> policies) {
+
         Queue<Policy> conflictingPolicies = new LinkedList<>(policies);
-        Collection<Policy> compiledPolicies = new LinkedList<>();
         while (!conflictingPolicies.isEmpty()) {
             Policy policy = conflictingPolicies.remove();
             Iterator<Policy> iterator2 = conflictingPolicies.iterator();
@@ -40,44 +73,65 @@ public class IntentCompilerImpl implements IntentCompiler {
                 Policy policy2 = iterator2.next();
                 if (conflicts(policy, policy2)) {
                     iterator2.remove();
-                    results.addAll(transform.resolve(policy, policy2));
+                    results.addAll(resolve(policy, policy2));
                 }
             }
             if (results.isEmpty()) {
-                compiledPolicies.add(policy);
+                addPolicy(policy);
             } else {
                 conflictingPolicies.addAll(results);
             }
         }
-        return compiledPolicies;
-    }
-
-    @Override
-    public Set<Endpoint> parseEndpointGroup(String csv)
-            throws UnknownHostException {
-        Set<Endpoint> endpoints = new LinkedHashSet<>();
-        String[] ipAdresses = csv.split(",");
-        for (String ipAddress : ipAdresses) {
-            endpoints.add(new EndpointImpl(InetAddress.getByName(ipAddress)));
-        }
-        return endpoints;
-    }
-
-    @Override
-    public Policy createPolicy(Set<Endpoint> source, Set<Endpoint> destination,
-            Set<Action> action) {
-        return new PolicyImpl(source, destination, action);
-    }
-
-    public IntentCompilerImpl() {
-        transform = new Transform();
+        return policyGraph;
     }
 
     private boolean conflicts(Policy p1, Policy p2) {
-        if (!Sets.intersection(p1.src(), p2.src()).isEmpty()
-                && !Sets.intersection(p1.dst(), p2.dst()).isEmpty()) {
+
+        if (p1.src().equals(p2.src()) && p1.dst().equals(p2.dst())) {
             return true;
         }
         return false;
+    }
+
+    private Collection<Policy> resolve(Policy p1, Policy p2) {
+        Collection<Policy> policies = new LinkedList<>();
+        policies.add(new PolicyImpl(p1.src(), p1.dst(), merge(p1.action(),
+                p2.action())));
+        return policies;
+
+    }
+
+    private Set<Action> merge(Set<Action> a1, Set<Action> a2) {
+        Set<Action> composebleActions = new LinkedHashSet<>();
+        Set<Action> observableActions = new LinkedHashSet<>();
+        Set<Action> exclusiveActions = new LinkedHashSet<>();
+        for (Action action : Sets.union(a1, a2)) {
+            switch (action.getType()) {
+                case COMPOSABLE:
+                    composebleActions.add(action);
+                    break;
+                case OBSERVER:
+                    observableActions.add(action);
+                    break;
+                case EXCLUSIVE:
+                    exclusiveActions.add(action);
+                    break;
+                default:
+                    return null;
+            }
+        }
+        if (!exclusiveActions.isEmpty()) {
+            if (exclusiveActions.size() == 1) {
+                return Sets.union(exclusiveActions, observableActions);
+            } else {
+                return null;
+            }
+        }
+        return Sets.union(composebleActions, observableActions);
+    }
+
+    @Override
+    public Policy createPolicy(Epg source, Epg destination, Set<Action> action) {
+        return new PolicyImpl(source, destination, action);
     }
 }
--- intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/api/IntentCompiler.java
+++ intentengine-compiler/src/main/java/org/opendaylight/nic/compiler/api/IntentCompiler.java
@@ -7,12 +7,19 @@
 //------------------------------------------------------------------------------
 package org.opendaylight.nic.compiler.api;
 
-import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.Set;
 
+import org.opendaylight.nic.compiler.Edge;
+import org.opendaylight.nic.compiler.Epg;
+
+import edu.uci.ics.jung.graph.DirectedGraph;
+
 public interface IntentCompiler {
-    Collection<Policy> compile(Collection<Policy> policies) throws IntentCompilerException;
-    Set<Endpoint> parseEndpointGroup(String csv) throws UnknownHostException;
-    Policy createPolicy(Set<Endpoint> source, Set<Endpoint> destination, Set<Action> action);
+    DirectedGraph<Epg, Edge> compile(Collection<Policy> policies)
+            throws IntentCompilerException;
+
+    // Set<Endpoint> parseEndpointGroup(String csv) throws UnknownHostException;
+
+    Policy createPolicy(Epg source, Epg destination, Set<Action> action);
 }
