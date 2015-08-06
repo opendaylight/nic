9da920707705d3b29cde6d1a95ff60cf5b099d8b
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-06 00:51:30 -0400

    Fix license violations in intentengine-impl
    Change-Id: I88b3dbbea1ec7267c14235c7a1e00619932576f8
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    (cherry picked from commit 71e5ea301c04fd31e7c35af9f876f6ae2502c2cd)

--- intentengine-impl/src/main/java/org/opendaylight/nic/api/NicConsoleProvider.java
+++ intentengine-impl/src/main/java/org/opendaylight/nic/api/NicConsoleProvider.java
@@ -1,14 +1,11 @@
-//------------------------------------------------------------------------------
-//  (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
-//
-//  Confidential computer software. Valid license from HP required for
-//  possession, use or copying.
-//
-//  Consistent with FAR 12.211 and 12.212, Commercial Computer Software,
-//  Computer Software Documentation, and Technical Data for Commercial Items
-//  are licensed to the U.S. Government under vendor's standard commercial
-//  license.
-//------------------------------------------------------------------------------
+/*
+ * Copyright (c) 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.nic.api;
 
 import java.util.List;
