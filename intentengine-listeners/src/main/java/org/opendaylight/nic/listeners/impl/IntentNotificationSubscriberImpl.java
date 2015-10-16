5e4d2a9ed684c94e788b35e0715d00f6b44c88c1
Saket Mahajani <saket.m.mahajani@hp.com>
2015-10-15 17:12:06 -0700

    Add support for various events - Support added for publishing and subscribing to intents, switches (Nodes) and links (NodeConnectors)
    Change-Id: I02944832329da349e3a43b14550cefe1bd273f0d
    Signed-off-by: Saket Mahajani <saket.m.mahajani@hp.com>

---
+++ intentengine-listeners/src/main/java/org/opendaylight/nic/listeners/impl/IntentNotificationSubscriberImpl.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 Hewlett-Packard Development Company and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.listeners.impl;
+
+import org.opendaylight.nic.of.renderer.api.FlowAction;
+import org.opendaylight.nic.of.renderer.api.OFRendererFlowService;
+import org.opendaylight.nic.listeners.api.IEventListener;
+import org.opendaylight.nic.listeners.api.IntentAdded;
+import org.opendaylight.nic.listeners.api.IntentRemoved;
+import org.opendaylight.nic.listeners.api.NicNotification;
+
+public class IntentNotificationSubscriberImpl implements IEventListener<NicNotification> {
+
+    OFRendererFlowService flowService;
+
+    IntentNotificationSubscriberImpl(OFRendererFlowService flowService) {
+        this.flowService = flowService;
+    }
+
+    @Override
+    public void handleEvent(NicNotification event) {
+        if (IntentAdded.class.isInstance(event)) {
+            IntentAdded addedEvent = (IntentAdded) event;
+            flowService.pushIntentFlow(addedEvent.getIntent(), FlowAction.ADD_FLOW);
+        }
+        if (IntentRemoved.class.isInstance(event)) {
+            IntentRemoved deleteEvent = (IntentRemoved) event;
+            flowService.pushIntentFlow(deleteEvent.getIntent(), FlowAction.REMOVE_FLOW);
+        }
+    }
+}
--- intentengine-listeners/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/listeners/rev150916/ListenerProviderModule.java
+++ intentengine-listeners/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/listeners/rev150916/ListenerProviderModule.java
@@ -1,5 +1,6 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.listeners.rev150916;
 
+import org.opendaylight.nic.listeners.impl.ListenerProviderImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +22,9 @@ public class ListenerProviderModule extends org.opendaylight.yang.gen.v1.urn.ope
 
     @Override
     public java.lang.AutoCloseable createInstance() {
+
+        final ListenerProviderImpl provider = new ListenerProviderImpl(getDataBrokerDependency(), getNotificationAdapterDependency());
+        provider.start();
         LOG.info("NIC Listeners started successfully.");
 
         return new AutoCloseable() {
@@ -28,6 +32,7 @@ public class ListenerProviderModule extends org.opendaylight.yang.gen.v1.urn.ope
             @Override
             public void close() throws Exception {
                 try {
+                    provider.close();
                 } catch (final Exception e) {
                     LOG.error("Unexpected error while stopping ListenerProviderModule", e);
                 }
