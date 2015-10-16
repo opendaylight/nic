7f264d934e3732982b97c39199f64327554a4b73
Saket Mahajani <saket.m.mahajani@hp.com>
2015-10-15 17:12:06 -0700

    Add support for various events - Support added for publishing and subscribing to intents, switches (Nodes) and links (NodeConnectors)
    Change-Id: I02944832329da349e3a43b14550cefe1bd273f0d
    Signed-off-by: Saket Mahajani <saket.m.mahajani@hp.com>

---
+++ intentengine-listeners/src/main/java/org/opendaylight/nic/listeners/impl/LinkDeletedImpl.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 Hewlett-Packard Development Company and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.listeners.impl;
+
+import org.opendaylight.nic.listeners.api.LinkDeleted;
+
+public class LinkDeletedImpl implements LinkDeleted {
+}
