71e71299caf02f2df57044b0f10ab4063ad24ce3
vinoth <m.vinoth@hcl.com>
2015-10-30 15:03:40 +0530

    Initial Draft: NIC+SFC Integration
    This patch contains a initial idea of SFC implementation code,
    and top of the patch need to add nic action code.
    
    The SFC code is available in ActionProvider class.
      1. Validating SFC Chain name.
      2. Getting the RendererServicePathFirstHop from SFC.
      3. Finally mapping an action name with rspFirstHop value into
         ConcurrenthMap.
    -------------------------------------------------
    
    Change-Id: I9d2602223edd7003a13811e1be13ecaea22054d4
    Signed-off-by: vinoth <m.vinoth@hcl.com>

---
+++ samples/action/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/action/impl/rev141210/ActionModule.java
@@ -0,0 +1,39 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.action.impl.rev141210;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.nic.action.impl.ActionProvider;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActionModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.action.impl.rev141210.AbstractActionModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ActionModule.class);
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    public ActionModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public ActionModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.action.impl.rev141210.ActionModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        LOG.info("Action Renderer createInstance()");
+        final ActionProvider provider = new ActionProvider(getDataBrokerDependency(), executor);
+        return provider;
+    }
+}
