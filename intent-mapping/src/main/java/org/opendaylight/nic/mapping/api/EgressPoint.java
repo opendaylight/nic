4bbe312d36c30c1ebe60b3b31e5084f79cd89c21
Icaro Camelo <icamelo@inocybe.com>
2015-11-03 10:11:07 -0500

    Added support for MPLS (draft)
    Change-Id: I5b4a6429d2344db26dec73f6370fd55ecde5acb0
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ intent-mapping/src/main/java/org/opendaylight/nic/mapping/api/EgressPoint.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.mapping.api;
+
+import java.io.IOException;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+
+public class EgressPoint extends MappedObject {
+
+    private static String EGRESS_POINT = "egress_point";
+    private String egressPoint;
+
+    @Override
+    public String toString() {
+        return "[type = " + EGRESS_POINT + ", value=" + egressPoint + "]";
+    }
+
+    public String getEgressPoint() {
+        return egressPoint == null ? "" : egressPoint;
+    }
+
+    public void setEgressPoint(String egressPoint) {
+        this.egressPoint = egressPoint;
+    }
+
+    @Override
+    public String getType() {
+        return EGRESS_POINT;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        out.writeUTF(egressPoint);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        egressPoint = in.readUTF();
+    }
+
+}
---
+++ intent-mapping/src/main/java/org/opendaylight/nic/mapping/api/MplsIngressLabel.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.mapping.api;
+
+import java.io.IOException;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+
+public class MplsIngressLabel extends MappedObject {
+
+    private static String MPLS_LABEL = "mpls_ingress_label";
+    private String label;
+
+    @Override
+    public String getType() {
+        return MPLS_LABEL;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        out.writeUTF(label);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        label = in.readUTF();
+    }
+
+    @Override
+    public String toString() {
+        return "[type = " + MPLS_LABEL + ", value=" + label + "]";
+    }
+
+    public String getLabel() {
+        return label == null ? "" : label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
