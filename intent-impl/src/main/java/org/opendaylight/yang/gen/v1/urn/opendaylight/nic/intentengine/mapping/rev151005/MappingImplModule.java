a9428a4bdc360564df03a9b78a5e1d78e4a1a5b1
adetalhouet <adetalhouet@inocybe.com>
2015-11-03 16:51:37 -0500

    Move intentengine to intent
    Change-Id: Ic313cb2a7f1b464cd396388d3ec097ad8c445e21
    Signed-off-by: adetalhouet <adetalhouet@inocybe.com>

---
+++ intent-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/intentengine/mapping/rev151005/MappingImplModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intentengine.mapping.rev151005;
+public class MappingImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intentengine.mapping.rev151005.AbstractMappingImplModule {
+    public MappingImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public MappingImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intentengine.mapping.rev151005.MappingImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
