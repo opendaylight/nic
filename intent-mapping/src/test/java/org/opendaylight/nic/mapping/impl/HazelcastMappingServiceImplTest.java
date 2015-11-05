fcaf940c248d20e48b4e6f910146ff25829bb499
Icaro Camelo <icamelo@inocybe.com>
2015-11-05 11:09:48 -0500

    Added support for MPLS (draft)
    Change-Id: I70f797ec066e84a4ecc3925d2398185356784ce3
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

--- intent-mapping/src/test/java/org/opendaylight/nic/mapping/impl/HazelcastMappingServiceImplTest.java
+++ intent-mapping/src/test/java/org/opendaylight/nic/mapping/impl/HazelcastMappingServiceImplTest.java
@@ -1,36 +1,31 @@
 package org.opendaylight.nic.mapping.impl;
 
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.opendaylight.nic.mapping.api.IntentMappingService;
-import org.opendaylight.nic.mapping.api.MappedObject;
-import org.opendaylight.nic.mapping.api.TypeHostname;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceRegistration;
-import org.powermock.api.mockito.PowerMockito;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.isA;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.nic.mapping.api.EgressPoint;
+import org.opendaylight.nic.mapping.api.IngressPoint;
+import org.opendaylight.nic.mapping.api.IntentMappingService;
+import org.opendaylight.nic.mapping.api.MappedObject;
+import org.opendaylight.nic.mapping.api.MplsEgressLabel;
+import org.opendaylight.nic.mapping.api.MplsIngressLabel;
+import org.opendaylight.nic.mapping.api.TypeHostname;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.powermock.api.mockito.PowerMockito;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HazelcastMappingServiceImplTest {
@@ -38,6 +33,7 @@ public class HazelcastMappingServiceImplTest {
     private HazelcastMappingServiceImpl service = new HazelcastMappingServiceImpl();
     private static String BOB = "bob";
     private static String ALICE = "alice";
+    private static String ADAM = "adam";
 
     private ServiceRegistration<IntentMappingService> nicConsoleRegistration;
 
@@ -55,8 +51,8 @@ public class HazelcastMappingServiceImplTest {
         ServiceRegistration<IntentMappingService> intentServiceMock = mock(ServiceRegistration.class);
 
         doReturn(mockBundleContext).when(service).getBundleCtx();
-        when(mockBundleContext.registerService(IntentMappingService.class,
-                service, null)).thenReturn(intentServiceMock);
+        when(mockBundleContext.registerService(IntentMappingService.class, service, null))
+                .thenReturn(intentServiceMock);
 
         TypeHostname hostname = new TypeHostname();
         hostname.setHostname("bob-server");
@@ -92,5 +88,45 @@ public class HazelcastMappingServiceImplTest {
         for (MappedObject o : objects) {
             assertTrue(o.getType().equals("hostname"));
         }
+    }
+
+    @Test
+    public final void testAddMPLSEndpointsInfo() throws Exception {
+
+        service = spy(service);
+        PowerMockito.mockStatic(FrameworkUtil.class);
+
+        BundleContext mockBundleContext = mock(BundleContext.class);
+        ServiceRegistration<IntentMappingService> intentServiceMock = mock(ServiceRegistration.class);
+
+        doReturn(mockBundleContext).when(service).getBundleCtx();
+        when(mockBundleContext.registerService(IntentMappingService.class, service, null))
+                .thenReturn(intentServiceMock);
+
+        List<MappedObject> list = new ArrayList<>();
+
+        MplsEgressLabel mplsEgressLabel = new MplsEgressLabel();
+        mplsEgressLabel.setLabel("21");
+
+        MplsIngressLabel mplsIngressLabel = new MplsIngressLabel();
+        mplsIngressLabel.setLabel("26");
+
+        IngressPoint ingPoint = new IngressPoint();
+        ingPoint.setIngressPoint("router1");
+
+        EgressPoint egPoint = new EgressPoint();
+        egPoint.setEgressPoint("router3");
+
+        list.add(mplsEgressLabel);
+        list.add(mplsIngressLabel);
+        list.add(ingPoint);
+        list.add(egPoint);
+
+        service.addList(ADAM, list);
+        Collection<MappedObject> objects = service.retrieve(ADAM);
+        assertNotNull(objects);
+        assertTrue(objects.size() == 4);
+
+        assertNotNull(service.stringRepresentation(ADAM));
     }
 }
