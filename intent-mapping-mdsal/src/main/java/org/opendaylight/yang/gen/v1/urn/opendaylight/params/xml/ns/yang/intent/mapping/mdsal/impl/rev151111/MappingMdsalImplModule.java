448ad82de2df1b2bf7dfe11ad8f8a2f991deb51c
Icaro Camelo <icamelo@inocybe.com>
2015-11-11 11:12:23 -0500

    Mapping service using MD-SAL
    Change-Id: I3ae489ea2b7cd9b7c26bc418bd8c89dbcea092bb
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

---
+++ intent-mapping-mdsal/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/intent/mapping/mdsal/impl/rev151111/MappingMdsalImplModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intent.mapping.mdsal.impl.rev151111;
+public class MappingMdsalImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intent.mapping.mdsal.impl.rev151111.AbstractMappingMdsalImplModule {
+    public MappingMdsalImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public MappingMdsalImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.intent.mapping.mdsal.impl.rev151111.MappingMdsalImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
