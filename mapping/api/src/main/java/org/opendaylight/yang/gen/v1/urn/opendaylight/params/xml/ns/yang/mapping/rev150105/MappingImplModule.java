11eb5ec9766c16551e359d512d263415f9dd5cdb
Icaro Camelo <icamelo@inocybe.com>
2015-11-11 11:12:23 -0500

    DO NOT MERGE: Mapping service using MD-SAL
    Change-Id: I3ae489ea2b7cd9b7c26bc418bd8c89dbcea092bb
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ mapping/api/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/mapping/rev150105/MappingImplModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.rev150105;
+public class MappingImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.rev150105.AbstractMappingImplModule {
+    public MappingImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public MappingImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.rev150105.MappingImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
