8abd19784407b4b3028e18d7af01ad75d0d433b8
Icaro Camelo <icamelo@inocybe.com>
2015-11-11 11:12:23 -0500

    DO NOT MERGE: Mapping service using MD-SAL
    Change-Id: I3ae489ea2b7cd9b7c26bc418bd8c89dbcea092bb
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ mapping/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/mapping/impl/rev151111/MappingModule.java
@@ -0,0 +1,39 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.impl.rev151111;
+
+import org.opendaylight.mapping.impl.MappingProvider;
+
+public class MappingModule extends
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.impl.rev151111.AbstractMappingModule {
+    public MappingModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public MappingModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+            org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mapping.impl.rev151111.MappingModule oldModule,
+            java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        final MappingProvider provider = new MappingProvider();
+        getBrokerDependency().registerProvider(provider);
+
+        final class AutoCloseableMapping implements AutoCloseable {
+
+            @Override
+            public void close() throws Exception {
+                provider.close();
+            }
+        }
+        return new AutoCloseableMapping();
+    }
+
+}
