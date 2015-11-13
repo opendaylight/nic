9521c13e3d70f5c5731a87f9eb585f0f24dc24c7
Icaro Camelo <icamelo@inocybe.com>
2015-11-13 13:27:20 -0500

    Mapping service abstraction
    Change-Id: I8f0b9766d19f32f7593bed670e33bf1a868b2547
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ intent-mapping-interface/src/main/java/org/opendaylight/nic/api/IntentMappingService.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Entreprise, Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.api;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface IntentMappingService extends java.lang.AutoCloseable {
+    void add(String key, String obj);
+
+    void addList(String key, List<String> objs);
+
+    Collection<String> retrieve(String key);
+
+    Collection<String> keys();
+
+    String stringRepresentation(String key);
+}
