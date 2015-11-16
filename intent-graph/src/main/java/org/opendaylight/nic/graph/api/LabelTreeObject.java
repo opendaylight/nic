6382ff6cb2459493561bba41b7de5ae5f10752c2
Anu Mercian <anu.mercian@hpe.com>
2015-11-16 14:12:55 -0800

    Updating pom files to include intent-graph module
    Revised yang model for intent graph for current master branch
    
    Initiated JAVA api/impl model
    
    Change-Id: I0ff25271b77ec6fa93eb99bfaa8856a3a47072c5
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/api/LabelTreeObject.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.api;
+
+public class LabelTreeObject {
+    /* Class for label tree objects: create instances of yang file for each. 
+     * Tenants
+     * Locations
+     * Applications
+     */
+}
