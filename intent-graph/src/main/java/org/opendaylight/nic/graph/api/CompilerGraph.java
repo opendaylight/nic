f4c3616252737035ef72364a24dd15c04daba229
Anu Mercian <anu.mercian@hpe.com>
2015-11-16 14:12:55 -0800

    Graph implementation.
    Revised yang model for intent graph for current master branch.
    
    Design of Input graph with 4 edge-types.
    
    Change-Id: I0ff25271b77ec6fa93eb99bfaa8856a3a47072c5
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/api/CompilerGraph.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.api;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.graph.Edges;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.graph.Nodes;
+import edu.uci.ics.jung.graph.DirectedGraph;
+import java.util.Collection;
+import java.util.Set;
+
+public interface CompilerGraph {
+
+    // TODO: To be extended to include the whitelist/blacklist composed model
+    DirectedGraph<Nodes, Edges> compile(Collection<InputGraph> graph)
+            throws CompilerGraphException;
+
+    /* creates an input graph with MD-SAL binding */
+    InputGraph createGraph(Nodes source, Nodes destination, Set<Edges> action);
+}
