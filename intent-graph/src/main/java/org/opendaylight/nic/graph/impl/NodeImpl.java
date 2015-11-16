f0cb5fc83372dc721623d9babb0337dc8b380df1
Anu Mercian <anu.mercian@hpe.com>
2015-11-16 14:12:55 -0800

    Graph implementation
    Revised yang model for intent graph for current master branch.
    
    Design of Input graph with 4 edge-types.
    
    Change-Id: I0ff25271b77ec6fa93eb99bfaa8856a3a47072c5
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/impl/NodeImpl.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.input.graph.Nodes;
+
+import java.net.InetAddress;
+
+public class NodeImpl {
+    /**
+     * Class of Node implementation to form a label-tree of high-level strings or low-level IP addresses
+     **/
+    // TODO: Extend this class to create label tree
+
+    InetAddress ipAddress;
+    Nodes macAddress;
+    int port;
+
+    public NodeImpl(InetAddress ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public NodeImpl(Nodes macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public NodeImpl(int port) {
+        this.port = port;
+    }
+
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    public Nodes getMacAddress() {
+        return macAddress;
+    }
+
+    public int getPortAddress() {
+        return port;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        NodeImpl endpoint = (NodeImpl) object;
+
+        return !(ipAddress != null ? !ipAddress.equals(endpoint.ipAddress) : endpoint.ipAddress != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return ipAddress != null ? ipAddress.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return ipAddress.getHostAddress();
+    }
+}
