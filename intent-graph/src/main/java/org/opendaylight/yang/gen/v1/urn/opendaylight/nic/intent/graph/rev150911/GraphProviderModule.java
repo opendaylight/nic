8fb3b322ae58a7fac75597837f7494b2077ddf07
Anu Mercian <anu.mercian@hpe.com>
2015-11-16 14:12:55 -0800

    Graph implementation
    Revised yang model for intent graph for current master branch
    
    Initiated JAVA api/impl model
    
    Change-Id: I0ff25271b77ec6fa93eb99bfaa8856a3a47072c5
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/intent/graph/rev150911/GraphProviderModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911;
+public class GraphProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.AbstractGraphProviderModule {
+    public GraphProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public GraphProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.GraphProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
