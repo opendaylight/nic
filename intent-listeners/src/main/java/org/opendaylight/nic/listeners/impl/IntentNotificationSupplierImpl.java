bc7df19da3ca32c234e0e44c415bee8b190102ac
Yrineu Rodrigues <yfrfelipe@gmail.com>
2015-11-18 00:30:19 -0300

    Adding Intent update event
    Change-Id: I477fed60bc90e90799d8cd2cbf14b95c5b875ac3
    Signed-off-by: Yrineu Rodrigues <yfrfelipe@gmail.com>

--- intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/IntentNotificationSupplierImpl.java
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/IntentNotificationSupplierImpl.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.nic.listeners.impl;
 
+import com.google.common.base.Preconditions;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.nic.listeners.api.EventType;
@@ -14,18 +15,18 @@ import org.opendaylight.nic.listeners.api.IEventListener;
 import org.opendaylight.nic.listeners.api.IEventService;
 import org.opendaylight.nic.listeners.api.IntentAdded;
 import org.opendaylight.nic.listeners.api.IntentRemoved;
+import org.opendaylight.nic.listeners.api.IntentUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-
-import com.google.common.base.Preconditions;
 
 /**
  * Implementation define a contract between {@link Intent} data object
  * and {@link IntentAdded} and {@link IntentRemoved} notifications.
  */
 public class IntentNotificationSupplierImpl  extends
-        AbstractNotificationSupplierItemRoot<Intent, IntentAdded, IntentRemoved> implements IEventService {
+        AbstractNotificationSupplierItemRoot<Intent, IntentAdded, IntentRemoved, IntentUpdated>
+        implements IEventService {
 
     private static final InstanceIdentifier<Intent> INTENT_IID =
             InstanceIdentifier.builder(Intents.class)
@@ -64,6 +65,13 @@ public class IntentNotificationSupplierImpl  extends
     }
 
     @Override
+    public IntentUpdated updateNotification(Intent object, InstanceIdentifier<Intent> path) {
+        Preconditions.checkArgument(object != null);
+        Preconditions.checkArgument(path != null);
+        return new IntentUpdateImpl(object);
+    }
+
+    @Override
     public void addEventListener(IEventListener listener) {
         serviceRegistry.registerEventListener(this, listener);
     }
@@ -84,13 +92,23 @@ public class IntentNotificationSupplierImpl  extends
     }
 
     @Override
+    public EventType getUpdateEventType() {
+        return EventType.INTENT_UPDATE;
+    }
+
+    @Override
     public Class getCreateImplClass() {
-        return IntentAdded.class;
+        return IntentAddedImpl.class;
     }
 
     @Override
     public Class getDeleteImplClass() {
-        return IntentRemoved.class;
+        return IntentRemovedImpl.class;
+    }
+
+    @Override
+    public Class getUpdateImplClass() {
+        return IntentUpdateImpl.class;
     }
 }
 
--- intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/NodeNotificationSupplierImpl.java
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/NodeNotificationSupplierImpl.java
@@ -8,6 +8,7 @@
 
 package org.opendaylight.nic.listeners.impl;
 
+import com.google.common.base.Preconditions;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.nic.listeners.api.EventType;
@@ -15,23 +16,23 @@ import org.opendaylight.nic.listeners.api.IEventListener;
 import org.opendaylight.nic.listeners.api.IEventService;
 import org.opendaylight.nic.listeners.api.NodeDeleted;
 import org.opendaylight.nic.listeners.api.NodeUp;
+import org.opendaylight.nic.listeners.api.NodeUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeUpdatedBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRemovedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeUpdatedBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Preconditions;
 
 /**
  * Implementation define a contract between {@link FlowCapableNode} data object
  * and {@link NodeUp} and {@link NodeDeleted} notifications.
  */
 public class NodeNotificationSupplierImpl extends
-        AbstractNotificationSupplierItemRoot<FlowCapableNode, NodeUp, NodeDeleted> implements IEventService {
+        AbstractNotificationSupplierItemRoot<FlowCapableNode, NodeUp, NodeDeleted, NodeUpdated> implements IEventService {
 
     private static final InstanceIdentifier<FlowCapableNode> FLOW_CAPABLE_NODE_IID =
             getNodeWildII().augmentation(FlowCapableNode.class);
@@ -79,6 +80,16 @@ public class NodeNotificationSupplierImpl extends
     }
 
     @Override
+    public NodeUpdated updateNotification(final FlowCapableNode flowCapableNode,
+                                          InstanceIdentifier<FlowCapableNode> path) {
+        Preconditions.checkArgument(flowCapableNode != null);
+        Preconditions.checkArgument(path != null);
+        final NodeUpdatedBuilder nodeUpdatedBuilder = new NodeUpdatedBuilder();
+        nodeUpdatedBuilder.setNodeRef(new NodeRef(path));
+        return new NodeUpdatedImpl(nodeUpdatedBuilder.getNodeRef());
+    }
+
+    @Override
     public void addEventListener(IEventListener listener) {
         serviceRegistry.registerEventListener(this, listener);
     }
@@ -99,6 +110,11 @@ public class NodeNotificationSupplierImpl extends
     }
 
     @Override
+    public EventType getUpdateEventType() {
+        return EventType.NODE_UPDATED;
+    }
+
+    @Override
     public Class getCreateImplClass() {
         return NodeUpImpl.class;
     }
@@ -106,6 +122,11 @@ public class NodeNotificationSupplierImpl extends
     @Override
     public Class getDeleteImplClass() {
         return NodeDeletedImpl.class;
+    }
+
+    @Override
+    public Class getUpdateImplClass() {
+        return NodeUpdatedImpl.class;
     }
 }
 
