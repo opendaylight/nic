38dec1bdb19540ad82f44982b6f26e0f5fc81d0c
Icaro Camelo <icamelo@inocybe.com>
2015-11-17 21:44:16 -0500

    MD-SAL mapping services implementation (add/get)
    Change-Id: I2d0341ba9cd862741739cd773ad0e0939fcb8544
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

--- intent-mapping-interface/src/main/java/org/opendaylight/nic/api/IntentMappingService.java
+++ intent-mapping-interface/src/main/java/org/opendaylight/nic/api/IntentMappingService.java
@@ -9,13 +9,33 @@ package org.opendaylight.nic.api;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface IntentMappingService extends AutoCloseable {
     void add(String key, String obj);
 
+    /**
+     * Index a map of objects based on a key.
+     *
+     * @param key
+     *            OuterKey
+     * @param objs
+     *            A map of inner elements
+     */
+    void add(String key, Map<String, String> objs);
+
     void addList(String key, List<String> objs);
 
     Collection<String> retrieve(String key);
+
+    /**
+     * Returns a map based on an indexed key.
+     *
+     * @param outerKey
+     *            indexed key
+     * @return A map of inner elements
+     */
+    Map<String, String> get(String outerKey);
 
     Collection<String> keys();
 
