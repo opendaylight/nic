4432951602f20012506849f0b74157b6ccf0aaf5
Saket Mahajani <saket.m.mahajani@hp.com>
2015-11-19 17:52:51 -0800

    Add support for event timestamping
    Change-Id: If1c4ad1deabfe22c035e7ec2e80a6e3e0469a677
    Signed-off-by: Saket Mahajani <saket.m.mahajani@hp.com>

--- intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/NodeUpdatedImpl.java
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/NodeUpdatedImpl.java
@@ -10,16 +10,27 @@ package org.opendaylight.nic.listeners.impl;
 import org.opendaylight.nic.listeners.api.NodeUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class NodeUpdatedImpl implements NodeUpdated {
 
     private NodeRef nodeRef;
+    private final Timestamp timeStamp;
 
     public NodeUpdatedImpl(NodeRef nodeRef) {
         this.nodeRef = nodeRef;
+        Date date= new Date();
+        timeStamp = new Timestamp(date.getTime());
     }
 
     @Override
     public NodeRef getNodeRef() {
         return getNodeRef();
+    }
+
+    @Override
+    public Timestamp getTimeStamp() {
+        return timeStamp;
     }
 }
