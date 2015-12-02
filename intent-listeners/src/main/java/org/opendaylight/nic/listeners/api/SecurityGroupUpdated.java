b7dd15de3c8ca505d83b02450bb052169f881a02
Saket Mahajani <saket.m.mahajani@hp.com>
2015-12-02 15:30:08 -0800

    Add support for Neutron security group and rule events
    Change-Id: I4fe07292cc7373f7d33e4a2121a82dbab356e5e6
    Signed-off-by: Saket Mahajani <saket.m.mahajani@hp.com>

---
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/api/SecurityGroupUpdated.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development LP.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.listeners.api;
+
+/**
+ * Interface to model the Neutron Security Group Updated Notification
+ */
+//TODO: Add relevant fields
+public interface SecurityGroupUpdated extends NicNotification {
+}
