07e621d1bab67057e2093edbd3bce10c5cf83d12
Saket Mahajani <saket.m.mahajani@hp.com>
2015-12-02 17:11:31 -0800

    Neutron-OVSDB integration bundle:
    Added initial skeleton code to handle Security Groups and Rules events
    
    Change-Id: I2ac02d179f27d6f507cfb07719d81151e5460417
    Signed-off-by: Saket Mahajani <saket.m.mahajani@hp.com>

---
+++ neutron-integration/src/main/java/org/opendaylight/nic/neutron.integration/impl/SecRuleNotificationSubscriberImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 Hewlett-Packard Development Company and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nic.neutron.integration.impl;
+
+import org.opendaylight.nic.listeners.api.*;
+
+public class SecRuleNotificationSubscriberImpl implements IEventListener<NicNotification> {
+
+    @Override
+    public void handleEvent(NicNotification event) {
+        if (SecurityRuleAdded.class.isInstance(event)) {
+            //TODO: Translate Security rules into intents
+        }
+        else if (SecurityRuleDeleted.class.isInstance(event)) {
+            //TODO
+        }
+        else if(SecurityRuleUpdated.class.isInstance(event)) {
+            //TODO:
+        }
+    }
+}
