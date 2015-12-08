a1de1355761b67f5c5e823e26027d725eca77882
Raphael Amorim <raphael.amorim@hp.com>
2015-12-07 17:21:19 -0800

    Adding support for multiple mapping service implementations
    Change-Id: I4b66b15a156305673d6c307bb08cde555eaa34cb
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

--- intent-impl/src/test/java/org/opendaylight/nic/impl/NicProviderTest.java
+++ intent-impl/src/test/java/org/opendaylight/nic/impl/NicProviderTest.java
@@ -33,6 +33,7 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.Test;
+import org.opendaylight.nic.api.IntentMappingService;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -93,6 +94,10 @@ public class NicProviderTest {
      */
     private NicProvider nicProvider;
     /**
+     * Instance of NicProvider to perform unit testing.
+     */
+    private IntentMappingService mappingService;
+    /**
      * It creates the required objects for every unit test cases.
      *
      * @throws Exception
@@ -108,10 +113,11 @@ public class NicProviderTest {
         mockWriteTransaction = mock(WriteTransaction.class);
         mockReadOnlyTransaction = mock(ReadOnlyTransaction.class);
         mockDataBroker = mock(DataBroker.class);
+        mappingService = mock(IntentMappingService.class);
         when(mockWriteTransaction.submit()).thenReturn(mockListenableFuture);
         when(mockDataBroker.newWriteOnlyTransaction()).thenReturn(mockWriteTransaction);
         when(mockDataBroker.newReadOnlyTransaction()).thenReturn(mockReadOnlyTransaction);
-        nicProvider = new NicProvider(mockDataBroker);
+        nicProvider = new NicProvider(mockDataBroker, mappingService);
     }
 
     /**
