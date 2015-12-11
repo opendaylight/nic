4cbf65e5806f707d5d62a946117cdc0d65bc5f2c
Anu Mercian <anu.mercian@hpe.com>
2015-12-11 14:42:09 -0800

    Composed Graph
    Initial commit for composed graph
    Graph Map Class
    
    Change-Id: I56903c81119fb16dd96080671316d39f461822d6
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>
    Signed-off-by: Alton Fong <alton.fong@hpe.com>
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>
    Signed-off-by: Alton Fong <alton.fong@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/impl/GraphMapImpl.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.opendaylight.nic.api.IntentMappingService;
+
+
+import java.util.Collection;
+
+/**
+ * Class to implement Label tree Map*/
+
+public class GraphMapImpl{
+
+    /**
+     * Labels incorporated: pga_label_tree
+     *         parent
+     *         children
+     */
+    protected IntentMappingService intentMappingService;
+
+    public GraphMapImpl(IntentMappingService mappingSvc) {
+        this.intentMappingService = mappingSvc;
+    }
+
+    //override add method to correct LabelImpl's parent and children declaration
+    //using Gson serializer to convert obj to string to be passed to intentmappingservice
+    public void addLabelChild(String key, String parent, String child) {
+        LabelImpl label = new LabelImpl(parent, child);
+
+        //use gson to convert label to json, then add to map
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String object = gson.toJson(label); //convert to string (Json form) to be added to map
+
+        intentMappingService.add(key, object); //add key/object to map
+
+        //check parent's children to make sure new obj is part of parent
+        Collection<String> parentJson = intentMappingService.retrieve(parent);
+        boolean delete = intentMappingService.delete(parent); //remove parent objects since they will be replaced with updated forms
+        if (parentJson != null && delete) {
+            for (String s : parentJson) {
+                LabelImpl parentObj = gson.fromJson(s, LabelImpl.class);
+                parentObj.addChild(key); //add children to parent (key)
+                //add back to map
+                String object2 = gson.toJson(parentObj);
+                intentMappingService.add(parent, object2);
+
+            }
+        }
+
+    }
+
+    public void addLabelChildren(String key, String parent, String[] children) {
+        LabelImpl label = new LabelImpl(parent, children);
+        //use gson to convert label to json, then add to map
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String object = gson.toJson(label); //convert to string (Json form) to be added to map
+
+        intentMappingService.add(key, object); //add key/object to map
+
+        //check parent's children to make sure new obj is part of parent
+        Collection<String> parentJson = intentMappingService.retrieve(parent);
+        boolean delete = intentMappingService.delete(parent); //remove parent objects since they will be replaced with updated forms
+        if (parentJson != null && delete) {
+            for (String s : parentJson) {
+                LabelImpl parentObj = gson.fromJson(s, LabelImpl.class);
+                parentObj.addChild(key); //add children to parent (key)
+                //add back to map
+                String object2 = gson.toJson(parentObj);
+                intentMappingService.add(parent, object2);
+
+            }
+        }
+    }
+
+
+}
