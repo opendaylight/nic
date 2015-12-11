25761a8dc0b954e16ba17bffc890d1cedf28a994
Anu Mercian <anu.mercian@hpe.com>
2015-12-11 14:42:09 -0800

    Label Tree Implementation
    EPG can be structured in a tree format, parent with children.
    This associated easy traversal to obtain relationship between EPGs
    
    Change-Id: I56903c81119fb16dd96080671316d39f461822d6
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>
    Signed-off-by: Alton Fong <alton.fong@hpe.com>
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/impl/GraphMapImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+
+import com.google.gson.Gson;
+import org.opendaylight.nic.mapping.api.IntentMappingService;
+
+import java.util.Collection;
+
+/**
+ * Class to implement Label tree Map*/
+
+public class GraphMapImpl{
+
+    /**
+     * Labels incorporated: pga_label_tree
+     *         parent
+     *         children
+     */
+    protected IntentMappingService intentMappingService;
+
+    public GraphMapImpl(IntentMappingService mappingSvc) {
+        this.intentMappingService = mappingSvc;
+    }
+
+    //override add method to correct LabelImpl's parent and children declaration
+    //using Gson serializer to convert obj to string to be passed to intentmappingservice
+    public void addLabelChild(String key, String parent, String child) {
+        LabelImpl label = new LabelImpl(parent, child, null);
+
+        //use gson to convert label to json, then add to map
+        Gson gson = new Gson();
+        String object = gson.toJson(label); //convert to string (Json form) to be added to map
+
+        intentMappingService.add(key, object); //add key/object to map
+
+        //check parent's children to make sure new obj is part of parent
+        Collection<String> parentJson = intentMappingService.retrieve(parent);
+        boolean delete = intentMappingService.delete(parent); //remove parent objects since they will be replaced with updated forms
+        if (parentJson != null && delete) {
+            for (String parentIndex : parentJson) {
+                LabelImpl parentObj = gson.fromJson(parentIndex, LabelImpl.class);
+                parentObj.addChild(key); //add children to parent (key)
+                //add back to map
+                String parent2Map = gson.toJson(parentObj);
+                intentMappingService.add(parent, parent2Map);
+
+            }
+        }
+
+    }
+
+    public void addLabelChildren(String key, String parent, String[] children) {
+        LabelImpl label = new LabelImpl(parent, children, null);
+        //use gson to convert label to json, then add to map
+        Gson gson = new Gson();
+        String object = gson.toJson(label); //convert to string (Json form) to be added to map
+
+        intentMappingService.add(key, object); //add key/object to map
+
+        //check parent's children to make sure new obj is part of parent
+        Collection<String> parentJson = intentMappingService.retrieve(parent);
+        boolean delete = intentMappingService.delete(parent); //remove parent objects since they will be replaced with updated forms
+        if (parentJson != null && delete) {
+            for (String parentIndex : parentJson) {
+                LabelImpl parentObj = gson.fromJson(parentIndex, LabelImpl.class);
+                parentObj.addChild(key); //add children to parent (key)
+                //add back to map
+                String parent2Map = gson.toJson(parentObj);
+                intentMappingService.add(parent, parent2Map);
+
+            }
+        }
+    }
+}
---
+++ intent-graph/src/test/java/org/opendaylight/nic/graph/impl/GraphMapImplTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.nic.mapping.api.IntentMappingService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.powermock.api.mockito.PowerMockito;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GraphMapImplTest {
+    protected IntentMappingService intentMappingService;
+
+    private GraphMapImpl service = new GraphMapImpl(intentMappingService);
+
+    @Before
+    public void setUp() throws Exception {
+        this.service = spy(service);
+        this.intentMappingService = mock(IntentMappingService.class);
+    }
+
+    //create a simple tree and test that each one is initialized properly with the right parent/children pairs.
+    //TODO: complete this test case
+    @Test
+    public final void testCreateGraph() throws Exception {
+        PowerMockito.mockStatic(FrameworkUtil.class);
+        BundleContext mockBundleContext = mock(BundleContext.class);
+        ServiceRegistration<GraphMapImpl> intentServiceMock = mock(ServiceRegistration.class);
+
+        //doReturn(mockBundleContext).when(GraphMapImpl.class);
+        when(mockBundleContext.registerService(GraphMapImpl.class, service, null))
+                .thenReturn(intentServiceMock);
+	/* TODO: Completion after the merge of IntentServiceMapping implementation */
+        //service.addLabelChild("apps", "Tnt", "app1");
+        //service.addLabelChildren("Tnt", "pga_label_tree", new String[]{"Dpts", "apps"}); //make null first
+        //service.addLabelChildren("Dpts", "Tnt", new String[]{"IT", "Engg"});
+        //service.addLabelChildren("app1", "apps", new String[]{"Web", "DB"});
+        //service.add("IT", "Dpts");
+        //service.add("Eng", "Dpts");
+        //service.add("web", "App1");
+        //service.add("db", "App1");
+        //System.out.println(service.intentMappingService.get("Tnt")); //get parent and children information
+        //System.out.println(service.intentMappingService.get("Dpts"));
+        //System.out.println(service.intentMappingService.get("apps"));
+    }
+}
