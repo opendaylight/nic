09532f2718ab7d7ae239333e6ce36435b45c8eea
Anu Mercian <anu.mercian@hpe.com>
2015-12-11 14:42:09 -0800

    Composed Graph
    Initial commit for composed graph
    Graph Map Class
    
    Change-Id: I56903c81119fb16dd96080671316d39f461822d6
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/impl/GraphMapImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+
+import org.opendaylight.nic.mapping.impl.HazelcastMappingServiceImpl;
+
+/**
+ * Class to implement Label tree Map*/
+
+public class GraphMapImpl extends HazelcastMappingServiceImpl{
+
+    /**
+     * Labels incorporated: pga_label_tree
+     *         parent
+     *         children
+     */
+}
