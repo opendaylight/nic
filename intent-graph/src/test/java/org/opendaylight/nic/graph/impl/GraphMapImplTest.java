7c08e32e3b51fdbc5ddcc33841944d8c908e87d5
Anu Mercian <anu.mercian@hpe.com>
2015-12-11 14:42:09 -0800

    Label Tree Implementation
    EPG can be structured in a tree format, parent with children.
    This associated easy traversal to obtain relationship between EPGs
    
    Change-Id: I56903c81119fb16dd96080671316d39f461822d6
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>
    Signed-off-by: Alton Fong <alton.fong@hpe.com>
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>
    Signed-off-by: Alton Fong <alton.fong@hpe.com>
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/test/java/org/opendaylight/nic/graph/impl/GraphMapImplTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.nic.mapping.api.IntentMappingService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.util.Collection;
+
+import static org.mockito.Mockito.*;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class GraphMapImplTest {
+    //private GraphMapImpl service = new GraphMapImpl();
+
+    //@Before
+    public void setUp() throws Exception {
+    }
+
+
+    //create a simple tree and test that each one is initialized properly with the right parent/children pairs.
+    //TODO: complete this test case
+    //@Test
+//    public final void testCreateGraph() {
+//        service = spy(service);
+//        PowerMockito.mockStatic(FrameworkUtil.class);
+//        BundleContext mockBundleContext = mock(BundleContext.class);
+//        ServiceRegistration<IntentMappingService> intentServiceMock = mock(ServiceRegistration.class);
+//
+//        doReturn(mockBundleContext).when(service).getBundleCtx();
+//        when(mockBundleContext.registerService(IntentMappingService.class, service, null))
+//                .thenReturn(intentServiceMock);
+//        service.add("Tnt", null, (String)null); //make null first
+//        service.add("Dpts", "Tnt", (String)null);
+//        service.add("apps", "Tnt", (String)null);
+//        service.add("App1", "apps");
+//        service.add("IT", "Dpts");
+//        service.add("Eng", "Dpts");
+//        service.add("web", "App1");
+//        service.add("db", "App1");
+//        System.out.println(service.getMultiMap().keySet()); // print list of keys
+//        for (String key : service.getMultiMap().keySet()) {
+//            Collection<LabelImpl> parentLabels = service.getMultiMap().get(key);
+//            for (LabelImpl parent: parentLabels)
+//                System.out.println(parent.getChildren());
+//
+//
+//        }
+//    }
+
+}
