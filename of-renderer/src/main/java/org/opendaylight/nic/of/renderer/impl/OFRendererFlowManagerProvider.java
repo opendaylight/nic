9fc0e896e9c67baaab92ebe24dd93c64dc2e3a2c
Rashmi Pujar <rpujar@inocybe.com>
2015-12-16 13:50:48 -0500

    Provide a class for MPLS VPN Intents actions
    Change-Id: I7559b85da095cbd0ae583cd2570fc14d1493fc45
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

--- of-renderer/src/main/java/org/opendaylight/nic/of/renderer/impl/OFRendererFlowManagerProvider.java
+++ of-renderer/src/main/java/org/opendaylight/nic/of/renderer/impl/OFRendererFlowManagerProvider.java
@@ -10,6 +10,7 @@ package org.opendaylight.nic.of.renderer.impl;
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
 
+import org.opendaylight.nic.api.IntentMappingService;
 import org.opendaylight.nic.of.renderer.api.OFRendererFlowService;
 import org.opendaylight.nic.of.renderer.api.OFRendererGraphService;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -28,6 +29,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.N
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,6 +51,7 @@ public class OFRendererFlowManagerProvider implements OFRendererFlowService, Aut
     private IntentFlowManager intentFlowManager;
     private ArpFlowManager arpFlowManager;
     private LldpFlowManager lldpFlowManager;
+    private IntentMappingService intentMappingService;
     private DataBroker dataBroker;
     private final PipelineManager pipelineManager;
 
@@ -66,6 +69,8 @@ public class OFRendererFlowManagerProvider implements OFRendererFlowService, Aut
         serviceRegistration.add(context.registerService(OFRendererGraphService.class,
                                    new NetworkGraphManager(),
                                    null));
+        ServiceReference<?> serviceReference = context.getServiceReference(IntentMappingService.class);
+        intentMappingService = (IntentMappingService) context.getService(serviceReference);
         intentFlowManager = new IntentFlowManager(dataBroker, pipelineManager);
         arpFlowManager = new ArpFlowManager(dataBroker, pipelineManager);
         lldpFlowManager = new LldpFlowManager(dataBroker, pipelineManager);
@@ -114,6 +119,30 @@ public class OFRendererFlowManagerProvider implements OFRendererFlowService, Aut
         return nodeMap;
     }
 
+    /**
+     * Creates a hashmap of the mapping information map for every subject
+     * @param endPointGroups :list of endpoint group from Intent request
+     * @return :double hashmap with mapping details of sujects
+     */
+    private Map<String, Map<String, String>> extractSubjectDetails(List<String> endPointGroups) {
+        Map<String, Map<String, String>> subjectsMapping = new HashMap<String, Map<String, String>>();
+        for (String id : endPointGroups) {
+            Map<String, String> values = null;
+            try {
+                values = intentMappingService.get(id);
+            } catch (Exception e) {
+                LOG.warn("No key found for {" + id + "} in IntentMappingService");
+            }
+
+            if (values != null) {
+                Map<String, String> innerKeys = intentMappingService.get(id);
+                if (innerKeys != null && innerKeys.size() > 0) {
+                    subjectsMapping.put(id, innerKeys);
+                }
+            }
+        }
+        return subjectsMapping;
+    }
     @Override
     public void close() throws Exception {
         for (ServiceRegistration<?> service: serviceRegistration) {
---
+++ of-renderer/src/test/java/org/opendaylight/nic/of/renderer/impl/MplsIntentFlowManagerTest.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.of.renderer.impl;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyShort;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.nic.of.renderer.utils.MatchUtils;
+import org.opendaylight.nic.pipeline_manager.PipelineManager;
+import org.opendaylight.nic.utils.FlowAction;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Instructions;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.actions.action.Allow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberMatcher;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@PrepareForTest({MatchUtils.class, MplsIntentFlowManager.class})
+@RunWith(PowerMockRunner.class)
+public class MplsIntentFlowManagerTest {
+    private static final String MPLS_INTENT_EXPECTED_STRING = "MPLS_Rule_";
+    private static final String EPG_SRC = "Site Src";
+    private static final String EPG_DST = "Site Dst";
+    private static final String MPLS_LABEL_KEY = "mpls_label";
+    private static final String IP_PREFIX_KEY = "ip_prefix";
+    private static final String SWITCH_PORT_KEY = "switch_port";
+    private List<String> endPointGroups = null;
+    private Map<String, Map<String, String>> subjectsMapping = null;
+    @Mock private Allow action;
+    private MplsIntentFlowManager mplsIntentFlowManager;
+
+    @Before
+    public void setUp() throws Exception {
+        mplsIntentFlowManager = mock(MplsIntentFlowManager.class, Mockito.CALLS_REAL_METHODS);
+        endPointGroups = new ArrayList<>();
+        endPointGroups.add(EPG_SRC);
+        endPointGroups.add(EPG_DST);
+
+        subjectsMapping = new HashMap<>();
+        Map<String, String> srcMapping = new HashMap<>();
+        srcMapping.put(MPLS_LABEL_KEY, "15");
+        srcMapping.put(IP_PREFIX_KEY, "10.0.0.1/32");
+        srcMapping.put(SWITCH_PORT_KEY, "openflow:1:2");
+        subjectsMapping.put(EPG_SRC, srcMapping);
+
+        Map<String, String> dstMapping = new HashMap<>();
+        dstMapping.put(MPLS_LABEL_KEY, "1");
+        dstMapping.put(IP_PREFIX_KEY, "10.0.0.2/32");
+        dstMapping.put(SWITCH_PORT_KEY, "openflow:2:2");
+        subjectsMapping.put(EPG_DST, dstMapping);
+
+        MemberModifier.field(MplsIntentFlowManager.class, "endPointGroups").set(mplsIntentFlowManager, endPointGroups);
+        MemberModifier.field(MplsIntentFlowManager.class, "subjectsMapping").set(mplsIntentFlowManager, subjectsMapping);
+        MemberModifier.field(MplsIntentFlowManager.class, "action").set(mplsIntentFlowManager, action);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testPushMplsFlow() throws Exception {
+        PowerMockito.whenNew(MatchBuilder.class).withNoArguments().thenReturn(mock(MatchBuilder.class));
+        FlowBuilder flowBldr = mock(FlowBuilder.class);
+        PowerMockito.whenNew(FlowBuilder.class).withNoArguments().thenReturn(flowBldr);
+        PowerMockito.whenNew(Ipv4Prefix.class).withAnyArguments().thenReturn(mock(Ipv4Prefix.class));
+
+        PowerMockito.mockStatic(MatchUtils.class);
+        PowerMockito.doNothing().when(MatchUtils.class, "createIPv4PrefixMatch", any(Ipv4Prefix.class), any(Ipv4Prefix.class), any(MatchBuilder.class));
+
+        Instructions buildedInstructions = mock(Instructions.class);
+        MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "createMPLSIntentInstructions", List.class, boolean.class, Short.class, String.class, boolean.class));
+        PowerMockito.when(mplsIntentFlowManager.createMPLSIntentInstructions(any(List.class), anyBoolean(), anyShort(), anyString(), anyBoolean())).thenReturn(buildedInstructions);
+
+        when(flowBldr.setInstructions(any(Instructions.class))).thenReturn(flowBldr);
+        MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "writeDataTransaction", NodeId.class, FlowBuilder.class, FlowAction.class));
+        when(mplsIntentFlowManager.writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class))).thenReturn(true);
+
+        mplsIntentFlowManager.pushMplsFlow(mock(NodeId.class), FlowAction.ADD_FLOW, "2");
+        verify(mplsIntentFlowManager).writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testPopMplsFlow() throws Exception {
+      PowerMockito.whenNew(MatchBuilder.class).withNoArguments().thenReturn(mock(MatchBuilder.class));
+      FlowBuilder flowBldr = mock(FlowBuilder.class);
+      PowerMockito.whenNew(FlowBuilder.class).withNoArguments().thenReturn(flowBldr);
+      PowerMockito.whenNew(Ipv4Prefix.class).withAnyArguments().thenReturn(mock(Ipv4Prefix.class));
+
+      PowerMockito.mockStatic(MatchUtils.class);
+      PowerMockito.doNothing().when(MatchUtils.class, "createIPv4PrefixMatch", any(Ipv4Prefix.class), any(Ipv4Prefix.class), any(MatchBuilder.class));
+
+      Instructions buildedInstructions = mock(Instructions.class);
+      MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "createMPLSIntentInstructions", List.class, boolean.class, Short.class, String.class, boolean.class));
+      PowerMockito.when(mplsIntentFlowManager.createMPLSIntentInstructions(any(List.class), anyBoolean(), anyShort(), anyString(), anyBoolean())).thenReturn(buildedInstructions);
+
+      when(flowBldr.setInstructions(any(Instructions.class))).thenReturn(flowBldr);
+      MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "writeDataTransaction", NodeId.class, FlowBuilder.class, FlowAction.class));
+      when(mplsIntentFlowManager.writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class))).thenReturn(true);
+
+      mplsIntentFlowManager.popMplsFlow(mock(NodeId.class), FlowAction.ADD_FLOW);
+      verify(mplsIntentFlowManager).writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testForwardMplsFlow() throws Exception {
+        PowerMockito.whenNew(MatchBuilder.class).withNoArguments().thenReturn(mock(MatchBuilder.class));
+        FlowBuilder flowBldr = mock(FlowBuilder.class);
+        PowerMockito.whenNew(FlowBuilder.class).withNoArguments().thenReturn(flowBldr);
+        PowerMockito.whenNew(Ipv4Prefix.class).withAnyArguments().thenReturn(mock(Ipv4Prefix.class));
+
+        PowerMockito.mockStatic(MatchUtils.class);
+        PowerMockito.doNothing().when(MatchUtils.class, "createIPv4PrefixMatch", any(Ipv4Prefix.class), any(Ipv4Prefix.class), any(MatchBuilder.class));
+
+        Instructions buildedInstructions = mock(Instructions.class);
+        MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "createMPLSIntentInstructions", List.class, boolean.class, Short.class, String.class, boolean.class));
+        PowerMockito.when(mplsIntentFlowManager.createMPLSIntentInstructions(any(List.class), anyBoolean(), anyShort(), anyString(), anyBoolean())).thenReturn(buildedInstructions);
+
+        when(flowBldr.setInstructions(any(Instructions.class))).thenReturn(flowBldr);
+        MemberModifier.suppress(MemberMatcher.method(MplsIntentFlowManager.class, "writeDataTransaction", NodeId.class, FlowBuilder.class, FlowAction.class));
+        when(mplsIntentFlowManager.writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class))).thenReturn(true);
+
+        mplsIntentFlowManager.forwardMplsFlow(mock(NodeId.class), FlowAction.ADD_FLOW, "2");
+        verify(mplsIntentFlowManager).writeDataTransaction(any(NodeId.class), any(FlowBuilder.class), any(FlowAction.class));
+    }
+
+    @Test
+    public void testCreateFlowName() {
+        MplsIntentFlowManager mplsIntentFlowManager = new MplsIntentFlowManager(mock(DataBroker.class), mock(PipelineManager.class));
+        mplsIntentFlowManager.setEndPointGroups(endPointGroups);
+        String flowName = mplsIntentFlowManager.createFlowName();
+        assertTrue(flowName.contains(MPLS_INTENT_EXPECTED_STRING));
+    }
+}
