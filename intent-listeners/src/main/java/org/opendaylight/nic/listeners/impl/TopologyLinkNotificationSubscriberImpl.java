4cebec77cd757f188ad21ceb3fbf4ca2522a7458
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-12-16 19:03:59 -0500

    Populate graph with Network-Topology Links
    Change-Id: I25560320b2120f3df99ebd9caba78b4eb5f5a9db
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

--- intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/TopologyLinkNotificationSubscriberImpl.java
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/impl/TopologyLinkNotificationSubscriberImpl.java
@@ -7,36 +7,46 @@
  */
 package org.opendaylight.nic.listeners.impl;
 
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.nic.listeners.api.IEventListener;
 import org.opendaylight.nic.listeners.api.NicNotification;
 import org.opendaylight.nic.listeners.api.TopologyLinkDeleted;
 import org.opendaylight.nic.listeners.api.TopologyLinkUp;
 import org.opendaylight.nic.of.renderer.api.OFRendererGraphService;
+import org.opendaylight.nic.utils.IidFactory;
+import org.opendaylight.nic.utils.MdsalUtils;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TopologyLinkNotificationSubscriberImpl implements IEventListener<NicNotification> {
 
     private OFRendererGraphService graphService;
+    private MdsalUtils mdsalUtils;
     private static final Logger LOG = LoggerFactory.getLogger(TopologyLinkNotificationSubscriberImpl.class);
 
-    public TopologyLinkNotificationSubscriberImpl(OFRendererGraphService graphService) {
+    public TopologyLinkNotificationSubscriberImpl(OFRendererGraphService graphService,
+                                                  MdsalUtils mdsalUtils) {
         this.graphService = graphService;
+        this.mdsalUtils = mdsalUtils;
     }
 
     @Override
     public void handleEvent(NicNotification event) {
         if (TopologyLinkUp.class.isInstance(event)) {
-            // TODO transaction to retrieve the Links
             LOG.trace("TOPOLOGY LINK ADDED");
+            Topology topo = mdsalUtils.read(LogicalDatastoreType.OPERATIONAL,
+                    IidFactory.getTopologyLinkInstanceIdentifier());
             if (graphService.getGraph() != null) {
-                // temp if
+                graphService.setLinks(topo.getLink());
             }
         }
         if (TopologyLinkDeleted.class.isInstance(event)) {
             LOG.trace("TOPOLOGY LINK REMOVED");
+            Topology topo = mdsalUtils.read(LogicalDatastoreType.OPERATIONAL,
+                    IidFactory.getTopologyLinkInstanceIdentifier());
             if (graphService.getGraph() != null) {
-                // temp if
+                graphService.setLinks(topo.getLink());
             }
         }
     }
