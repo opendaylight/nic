1e846562bf254e1e86e837493fab2cdcc8ead6e0
Renato Aguiar <renato.aguiar@hpe.com>
2015-12-16 16:51:06 -0800

    Refactoring of the intent-mapping-service to support multiple backends
    Centralizing the interface/yang for the service on a separate bundle (intent-mapping-interface)
    and the implementations in other bundles. Also add the mapping service reference to the main
    (intent-impl) bundle.
    
    Change-Id: I3a152be1e535f2f13eddf3f841cb480aeb96ae15
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

--- intent-mapping-interface/src/main/java/org/opendaylight/nic/mapping/api/IntentMappingService.java
+++ intent-mapping-interface/src/main/java/org/opendaylight/nic/mapping/api/IntentMappingService.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.nic.api;
+package org.opendaylight.nic.mapping.api;
 
 import java.util.Collection;
 import java.util.List;
