95088f07c856f3f0d4e89b67f8583d052f45af1c
Anu Mercian <anu.mercian@hpe.com>
2015-12-22 12:03:42 -0800

    Classifiers for Graph
    Work in Progress
    
    Change-Id: I50a0f18c7c5a6cb6fa3b4c791590f5ea0450004b
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-graph/src/main/java/org/opendaylight/nic/graph/impl/ClassifierImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.graph.impl;
+
+import org.opendaylight.nic.graph.api.Classifer;
+
+public class ClassifierImpl implements Classifer{
+    /* Class to implement classifier map to application port numbers */
+}
