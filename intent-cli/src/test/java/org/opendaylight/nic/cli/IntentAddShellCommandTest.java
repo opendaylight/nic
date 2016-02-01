302740276e6e2286ec7089b7fa82cc799b7b2986
Anu Mercian <anu.mercian@hpe.com>
2016-02-01 14:37:02 -0800

    Mapped Objects for Classifier and QoS Constraint
    - BUG 3956:: Fix for checking intent subjects and constraints in mapping services
    - Show Classifiers in intent:show command
    - Add classifiers in intent:add command and check for classifiers
    - Notification to add unknown labels in the mapping services
    
    Change-Id: Ia6ff6730a3f22bbe5a27171fb17e17fded096a0e
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>
    Signed-off-by: Raphael Amorim <raphael.amorim@hp.com>

--- intent-cli/src/test/java/org/opendaylight/nic/cli/IntentAddShellCommandTest.java
+++ intent-cli/src/test/java/org/opendaylight/nic/cli/IntentAddShellCommandTest.java
@@ -1,14 +1,6 @@
 package org.opendaylight.nic.cli;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import java.util.List;
-
-import static org.junit.Assert.*;
-
+import com.google.gson.Gson;
 import org.apache.felix.service.command.CommandSession;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,31 +8,41 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.nic.api.NicConsoleProvider;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
+import org.opendaylight.nic.constraints.QualityOfServiceConstraint;
+import org.opendaylight.nic.mapped.MappedObject;
+import org.opendaylight.nic.mapping.api.IntentMappingService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Actions;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Constraints;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Subjects;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.types.rev150122.Uuid;
+
+import java.io.InvalidObjectException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IntentAddShellCommandTest {
 
     @Mock private NicConsoleProvider nicConsole;
+    @Mock private IntentMappingService mappingService;
     @Mock private CommandSession cmdSession;
 
     private IntentAddShellCommand nicAddShellCmd;
 
     @Before
     public void setUp() throws Exception {
-        nicAddShellCmd = new IntentAddShellCommand(nicConsole);
+        nicAddShellCmd = new IntentAddShellCommand(nicConsole, mappingService);
     }
 
     @Test
     public final void testDoExecute() throws Exception {
         Object feedBack = nicAddShellCmd.doExecute();
             assertNotNull(feedBack);
-            assertEquals("As we test a mocked object the feedback should be error message","Error creating new intent", feedBack);
+            assertEquals("As we test a mocked object the feedback should be error message","No subject found with value: any in the mapping service. Try adding it using: intent:map' command", feedBack);
     }
 
     @Test
@@ -51,7 +53,14 @@ public class IntentAddShellCommandTest {
     }
 
     @Test
-    public final void testCreateSubjects() {
+    public final void testCreateSubjects() throws InvalidObjectException {
+
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("any","0.0.0.0");
+
+        when(mappingService.get("any")).thenReturn(resultMap);
+        when(mappingService.get("any")).thenReturn(resultMap);
+
         List<Subjects> subjects = nicAddShellCmd.createSubjects();
         assertNotNull(subjects);
         assertEquals("there are should be at least 2 subjects", true , subjects.size() > 1);
@@ -66,7 +75,20 @@ public class IntentAddShellCommandTest {
      * Test case for {@link IntentAddShellCommand#createConstraints()}.
      */
     @Test
-    public final void testCreateConstraints() {
+    public final void testCreateConstraints() throws InvalidObjectException {
+        Gson gson = new Gson();
+
+        MappedObject qos = new QualityOfServiceConstraint("HTTP");
+        qos.setKey("HTTP");
+        String object = gson.toJson(qos, MappedObject.class);
+
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("QOS", object);
+        mappingService.add("QOS", resultMap);
+
+        when(mappingService.get("QOS")).thenReturn(resultMap);
+        when(mappingService.get("QOS")).thenReturn(resultMap);
+
         List<Constraints> constraints = nicAddShellCmd.createConstraints();
         assertNotNull(constraints);
         assertEquals("there are should be at least 1 constraints", true , constraints.size() > 0);
