966eb21f6cf5c80ff6c81942b0b02ebe155c043c
Anu Mercian <anu.mercian@hpe.com>
2016-02-04 14:20:41 -0800

    Listeners for Graph Edge to be used in State machine
    Added a field to store Intent UUID in the graph. This is assist all compiled policies
    to store UUIDs of compiled policies in the resulting Graph MD-SAL.
    
    Change-Id: I0e717bfeaa99746ce262d35e60cb05407ddbd8bf
    Signed-off-by: Anu Mercian <anu.mercian@hpe.com>

---
+++ intent-listeners/src/main/java/org/opendaylight/nic/listeners/api/GraphEdgeAdded.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Hewlett Packard Enterprise Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.nic.listeners.api;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.graph.rev150911.graph.Edges;
+
+import java.util.Set;
+
+/** Interface for edge listener
+ */
+public interface GraphEdgeAdded extends NicNotification {
+
+    /** Method to get edges
+     * @return Collection of edges
+     */
+    Set<Edges> getEdge();
+}
