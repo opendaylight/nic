3e035eee2d239635b3c490f854d046817bb0c6c7
Hideyuki Tai <Hideyuki.Tai@necam.com>
2016-02-18 12:16:03 -0800

    WIP: Bug 5389: Add intents to OPERATIONAL data store.
    When VTN Renderer successfully handles an intent, it is assumed to
    update the OPERATIONAL data store.  However, the way of VTN Renderer
    to update OPERATIONAL data store was wrong. It put intents to the
    CONFIGURATION data store. And, the VTN Renderer updated the data store
    in an inefficient way.
    
     - Fixed bug 5389, that is, made VTN Renderer to put intents to
       OPERATIONAL data store with status information.
     - Improved the way to update the data store.
     - Fixed wrong comments.
     - Deleted the unused private method "listIntents" from the
       VTNRendererUtility class.
    
    Change-Id: I541f64396301b5a160fa84380bfa696929124388
    Signed-off-by: Hideyuki Tai <Hideyuki.Tai@necam.com>

--- vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRendererUtility.java
+++ vtn-renderer/src/main/java/org/opendaylight/nic/vtn/renderer/VTNRendererUtility.java
@@ -19,12 +19,14 @@ import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.nic.utils.MdsalUtils;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intent.Status;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.Intents;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.IntentsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.IntentBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.IntentKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -152,58 +154,19 @@ public class VTNRendererUtility {
     }
 
     /**
-     * Executes put as a blocking transaction.
+     * Add an operational information of an intent into the OPERATIONAL data store.
      *
-     * @param intent the intent instance
-     * @param isStatus the intent status.
-     * @return {@code = true} the intent is added successfully.
+     * @param intent  The intent to be added into the OPERATIONAL data store.
+     * @param status  The intent status.
+     *
+     * @return {@code true} if the intent is added successfully.
      */
-    public boolean addIntent(Intent intent, Status isStatus) {
-        Intents intents;
-        List<Intent> listOfIntents = listIntents();
-        try {
-            InstanceIdentifier<Intents> identifier = InstanceIdentifier.builder(Intents.class).build();
-            Intent intentData = new IntentBuilder().setId(intent.getId()).setStatus(isStatus).build();
-
-            listOfIntents.add(intentData);
-            intents = new IntentsBuilder().setIntent(listOfIntents).build();
-            WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
-            tx.put(LogicalDatastoreType.CONFIGURATION, identifier, intents);
-            tx.submit();
-        } catch (Exception e) {
-            log.error("Add Intents: failed: {}", e);
-            return false;
-        }
-        log.info("The intent status is inserted successfully");
-        return true;
-    }
-
-    /**
-     * Executes read as a blocking transaction.
-     * @return the result as the data object requested
-     */
-    private List<Intent> listIntents() {
-        Optional<Intents> optionalDataObject;
-        try {
-            InstanceIdentifier<Intents> identifier = InstanceIdentifier
-                    .builder(Intents.class).build();
-            ReadOnlyTransaction tx = dataBroker.newReadOnlyTransaction();
-            CheckedFuture<Optional<Intents>, ReadFailedException> future = tx
-                    .read(LogicalDatastoreType.CONFIGURATION, identifier);
-            try {
-                optionalDataObject = future.checkedGet();
-                if (optionalDataObject.isPresent()) {
-                    listOfIntents = optionalDataObject.get().getIntent();
-                }
-            } catch (ReadFailedException ex) {
-                log.error("List Intents: inner catch failed: {}", ex);
-                return null;
-            }
-        } catch (Exception e) {
-            log.error("List of Intents: failed: {}", e);
-            return null;
-        }
-        log.info("List of Intents retrieved successfully");
-        return listOfIntents;
+    public boolean addIntent(Intent intent, Status status) {
+        MdsalUtils mdsal = new MdsalUtils(dataBroker);
+        InstanceIdentifier<Intent> identifier = InstanceIdentifier.builder(Intents.class).
+            child(Intent.class, new IntentKey(intent.getId())).
+            build();
+        Intent operationalIntent = new IntentBuilder(intent).setStatus(status).build();
+        return mdsal.put(LogicalDatastoreType.OPERATIONAL, identifier, operationalIntent);
     }
 }
