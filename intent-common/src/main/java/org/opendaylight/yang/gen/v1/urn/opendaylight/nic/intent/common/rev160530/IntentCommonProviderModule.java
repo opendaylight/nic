1d040176922c01bbc06c48ff6f8383f4aee8d943
Yrineu Rodrigues <yfrfelipe@gmail.com>
2016-07-30 21:48:21 -0300

    Creating intent-common module
    - Adding Transaction listeners
    - Adding EventTypes
    
    Change-Id: I95a79bcc4204cde6b6d97eac086490c0d43cd4ae
    Signed-off-by: Yrineu Rodrigues <yfrfelipe@gmail.com>
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>
    Signed-off-by: Icaro Camelo <icarorvc@gmail.com>

---
+++ intent-common/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/intent/common/rev160530/IntentCommonProviderModule.java
@@ -0,0 +1,31 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.common.rev160530;
+
+import org.opendaylight.nic.transaction.api.IntentCommonProviderService;
+import org.opendaylight.nic.transaction.impl.IntentCommonProviderServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IntentCommonProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.common.rev160530.AbstractIntentCommonProviderModule {
+    public IntentCommonProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+    private static final Logger LOG = LoggerFactory.getLogger(IntentCommonProviderModule.class);
+
+    public IntentCommonProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.nic.intent.common.rev160530.IntentCommonProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        LOG.info("Starting Intent-Common module.");
+        final IntentCommonProviderService commonProvider = new IntentCommonProviderServiceImpl();
+        commonProvider.start();
+        return commonProvider;
+    }
+
+}
