d39ae077b8b81ca273702758b2b4a950708d5c38
Icaro Camelo <icarorvc@gmail.com>
2016-10-05 21:45:44 -0400

    intent-engine onf nbi compliant
    Change-Id: I7f8c72858ff44407ac4fd9f8222d29862af81d35
    Signed-off-by: Icaro Camelo <icarorvc@gmail.com>

---
+++ intent-engine/src/main/java/TranslatorService.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 Open Networking Foundation and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+import org.opendaylight.yang.gen.v1.urn.onf.intent.intent.nbi.rev160920.intent.definitions.Association;
+import org.opendaylight.yang.gen.v1.urn.onf.intent.intent.nbi.rev160920.intent.definitions.AssociationsBuilder;
+
+public class TranslatorService {
+    public void test(){
+
+    }
+}
