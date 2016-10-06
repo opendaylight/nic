afeab14a6a75a95db4355001a58583cf59089b61
Icaro Camelo <icarorvc@gmail.com>
2016-10-05 21:45:44 -0400

    Intent-engine (ONF Intent-NBI) compliant
    Change-Id: I7f8c72858ff44407ac4fd9f8222d29862af81d35
    Signed-off-by: Icaro Camelo <icarorvc@gmail.com>

---
+++ intent-engine/src/main/java/org/opendaylight/yang/gen/v1/urn/onf/intent/intent/nbi/rev160920/IntentEngineProviderModule.java
@@ -0,0 +1,33 @@
+package org.opendaylight.yang.gen.v1.urn.onf.intent.intent.nbi.rev160920;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.opendaylight.nic.impl.IntentEngineProviderImpl;
+
+public class IntentEngineProviderModule extends org.opendaylight.yang.gen.v1.urn.onf.intent.intent.nbi.rev160920.AbstractIntentEngineProviderModule {
+    private static final Logger LOG = LoggerFactory
+            .getLogger(IntentEngineProviderModule.class);
+    public IntentEngineProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public IntentEngineProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.onf.intent.intent.nbi.rev160920.IntentEngineProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        LOG.info("Starting Intent-Engine module.");
+        final DataBroker dataBroker = getDataBrokerDependency();
+        final IntentEngineProviderImpl commonProvider = new IntentEngineProviderImpl(dataBroker);
+        commonProvider.init();
+        return commonProvider;
+    }
+
+}
