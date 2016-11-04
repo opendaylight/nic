a3bb2b41aa1a10ed5e070136aacc61991b542264
Victor Mota <victor_mota@atlantico.com.br>
2016-11-04 16:01:39 -0300

    [WIP] Increase of-renderer coverage
        OFRendererFlowManagerProvider.java
    
    Change-Id: Ied42033ac1003719a9c68d625444627fca8b8ad1
    Signed-off-by: Victor Mota <victor_mota@atlantico.com.br>

---
+++ of-renderer/src/main/java/org/opendaylight/nic/of/renderer/strategy/RedirectExecutor.java
@@ -0,0 +1,32 @@
+*
+ * Copyright (c) 2015 - 2016 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+package org.opendaylight.nic.of.renderer.strategy;
+
+import org.opendaylight.nic.of.renderer.impl.RedirectFlowManager;
+import org.opendaylight.nic.utils.FlowAction;
+import org.opendaylight.nic.utils.exceptions.IntentInvalidException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
+
+/**
+ * @author victor. Created on 11/09/16.
+ */
+public class RedirectExecutor implements ActionStrategy {
+
+    private RedirectFlowManager redirectFlowManager;
+
+    public RedirectExecutor(RedirectFlowManager redirectFlowManager) {
+        this.redirectFlowManager = redirectFlowManager;
+    }
+
+    @Override
+    public void execute(Intent intent, FlowAction flowAction)
+            throws IntentInvalidException {
+        redirectFlowManager.redirectFlowConstruction(intent, flowAction);
+    }
+
+}
