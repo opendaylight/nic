d6619aa3cfce49b95de8bdb18ea3b94fdbc40324
Victor Mota <victor_mota@atlantico.com.br>
2016-11-04 16:01:39 -0300

    Increase of-renderer coverage
        OFRendererFlowManagerProvider.java
    
    Change-Id: Ied42033ac1003719a9c68d625444627fca8b8ad1
    Signed-off-by: Victor Mota <victor_mota@atlantico.com.br>

--- of-renderer/src/test/java/org/opendaylight/nic/of/renderer/impl/OFRendererFlowManagerProviderTest.java
+++ of-renderer/src/test/java/org/opendaylight/nic/of/renderer/impl/OFRendererFlowManagerProviderTest.java
@@ -7,9 +7,12 @@
  */
 package org.opendaylight.nic.of.renderer.impl;
 
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
 import org.opendaylight.nic.mapping.api.IntentMappingService;
@@ -19,19 +22,24 @@ import org.opendaylight.nic.pipeline_manager.PipelineManager;
 import org.opendaylight.nic.utils.IntentUtils;
 import org.opendaylight.nic.utils.exceptions.IntentElementNotFoundException;
 import org.opendaylight.nic.utils.exceptions.IntentInvalidException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Actions;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.Subjects;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.actions.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.actions.action.Redirect;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.EndPointGroup;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intents.Intent;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.types.rev150122.Uuid;
 import org.opendaylight.yangtools.concepts.Registration;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
 import org.powermock.api.mockito.PowerMockito;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.UUID;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -46,64 +54,194 @@ public class OFRendererFlowManagerProviderTest {
     private IntentFlowManager intentFlowManager;
     private ArpFlowManager arpFlowManager;
     private LldpFlowManager lldpFlowManager;
-    private IntentMappingService intentMappingService;
-    private DataBroker dataBroker;
-    private PipelineManager pipelineManager;
+
     private OFRendererGraphService ofRendererGraphService;
     private MplsIntentFlowManager mplsIntentFlowManager;
     private QosConstraintManager qosConstraintManager;
     private Registration registration;
     private RedirectFlowManager redirectFlowManager;
     private Subject subject;
-    private NotificationProviderService notificationProviderService;
-    private Intent intent;
+
+    @Mock
     private EndPointGroup source;
-    private EndPointGroup target;
+
+    @Mock
+    private org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.end.point.group.EndPointGroup sourceEndPointGroup;
+
+    @Mock
+    Subjects subjectsSource;
+
+    @Mock
+    private EndPointGroup destination;
+
+    @Mock
+    org.opendaylight.yang.gen.v1.urn.opendaylight.intent.rev150122.intent.subjects.subject.end.point.group.EndPointGroup dstEndPointGroup;
+
+    @Mock
+    Subjects subjectsDestination;
+
+    @Mock
+    private List<Actions> actionsList;
+
+    @Mock
+    private Actions actions;
+
+    @Mock
+    private Action action;
+    
+    @Mock
+    Redirect redirect;
+
+    @Mock
+    private DataBroker dataBroker;
+
+    @Mock
+    private PipelineManager pipelineManager;
+
+    @Mock
+    private NotificationProviderService notificationProviderService;
+
+    @Mock
+    private Intent intent;
+
+    @Mock
+    private Uuid uuidMock;
+
+    @Mock
+    private IntentMappingService intentMappingService;
 
     private OFRendererFlowManagerProvider ofRendererFlowManagerProvider;
+    private final String DEFAULT_STR_UUID = UUID.randomUUID().toString();
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(uuidMock.getValue()).thenReturn(DEFAULT_STR_UUID);
+        initIntentWithSourceAndDestination();
         ofRendererGraphService = spy(new NetworkGraphManager());
-        dataBroker = mock(DataBroker.class);
-        pipelineManager = mock(PipelineManager.class);
         mplsIntentFlowManager = spy(new MplsIntentFlowManager(dataBroker, pipelineManager));
         arpFlowManager = spy(new ArpFlowManager(dataBroker, pipelineManager));
         lldpFlowManager = spy(new LldpFlowManager(dataBroker, pipelineManager));
         qosConstraintManager = spy(new QosConstraintManager(dataBroker, pipelineManager));
-        notificationProviderService = mock(NotificationProviderService.class);
-        intent = mock(Intent.class);
         ofRendererFlowManagerProvider = spy(new OFRendererFlowManagerProvider(dataBroker, pipelineManager,
                 intentMappingService, notificationProviderService));
     }
 
-    @Test (expected = IntentInvalidException.class)
-    public void testIsMPLSWithInvalidIntent() throws IntentInvalidException {
-        ofRendererFlowManagerProvider.isMPLS(intent);
+    private void initIntentWithSourceAndDestination() {
+        when(sourceEndPointGroup.getName()).thenReturn("source");
+        when(source.getEndPointGroup()).thenReturn(sourceEndPointGroup);
+        when(dstEndPointGroup.getName()).thenReturn("destination");
+        when(destination.getEndPointGroup()).thenReturn(dstEndPointGroup);
+        when(subjectsSource.getSubject()).thenReturn(source);
+        when(subjectsDestination.getSubject()).thenReturn(destination);
+
+        List<Subjects> subjectsList = spy(new ArrayList<>());
+        subjectsList.add(subjectsSource);
+        subjectsList.add(subjectsDestination);
+
+        when(intent.getSubjects()).thenReturn(subjectsList);
+        when(intent.getId()).thenReturn(uuidMock);
+        
+        Mockito.when(intent.getActions()).thenReturn(actionsList);
+        Mockito.when(actionsList.get(0)).thenReturn(actions);
     }
 
-    @Test(expected = IntentInvalidException.class)
-    public void test() throws Exception {
-        PowerMockito.mockStatic(IntentUtils.class);
-        List<EndPointGroup> endPointGroups = spy(new ArrayList<>());
-        List<Subjects> subjectses = spy(new ArrayList<>());
-        when(intent.getSubjects()).thenReturn(subjectses);
-        source = mock(EndPointGroup.class);
-        target = mock(EndPointGroup.class);
-        endPointGroups.add(source);
-        endPointGroups.add(target);
-        when(endPointGroups.get(0)).thenReturn(source);
-        when(endPointGroups.get(1)).thenReturn(target);
-        PowerMockito.when(IntentUtils.class, "extractEndPointGroup", endPointGroups, 0).thenReturn(source);
-        PowerMockito.when(IntentUtils.class, "extractEndPointGroup", endPointGroups, 1).thenReturn(target);
+//    @Test (expected = IntentInvalidException.class)
+//    public void testIsMPLSWithInvalidIntent() throws IntentInvalidException {
+//        ofRendererFlowManagerProvider.isMPLS(intent);
+//    }
 
-        when(IntentUtils.extractSrcEndPointGroup(intent)).thenReturn(source);
-        when(IntentUtils.extractDstEndPointGroup(intent)).thenReturn(target);
-        ofRendererFlowManagerProvider.isMPLS(intent);
-    }
+//    @Test(expected = IntentInvalidException.class)
+//    public void test() throws Exception {
+//        PowerMockito.mockStatic(IntentUtils.class);
+//
+//        List<EndPointGroup> endPointGroups = spy(new ArrayList<>());
+//        List<Subjects> subjectses = spy(new ArrayList<>());
+//
+//        when(intent.getSubjects()).thenReturn(subjectses);
+//
+//        endPointGroups.add(source);
+//        endPointGroups.add(destination);
+//
+//        when(endPointGroups.get(0)).thenReturn(source);
+//        when(endPointGroups.get(1)).thenReturn(destination);
+//
+//        PowerMockito.when(IntentUtils.class, "extractEndPointGroup", endPointGroups, 0).thenReturn(source);
+//        PowerMockito.when(IntentUtils.class, "extractEndPointGroup", endPointGroups, 1).thenReturn(destination);
+//
+//        when(IntentUtils.extractSrcEndPointGroup(intent)).thenReturn(source);
+//        when(IntentUtils.extractDstEndPointGroup(intent)).thenReturn(destination);
+//        ofRendererFlowManagerProvider.isMPLS(intent);
+//    }
 
-    @Test(expected = IntentElementNotFoundException.class)
-    public void testIsRedirect() {
+    @Test(expected = NoSuchElementException.class)
+    public void testIsRedirectWithNoSuchElementException() {
+        Mockito.when(actions.getAction()).thenReturn(null);
         ofRendererFlowManagerProvider.isRedirect(intent);
+    }
+
+    @Test
+    public void testIsRedirect() {
+        Mockito.when(actions.getAction()).thenReturn(redirect);
+        Assert.assertTrue(ofRendererFlowManagerProvider.isRedirect(intent));
+    }
+
+    @Test
+    public void testIsRedirectFalse() {
+        Mockito.when(actions.getAction()).thenReturn(action);
+        Assert.assertFalse(ofRendererFlowManagerProvider.isRedirect(intent));
+    }
+
+    @Test(expected=IntentElementNotFoundException.class)
+    public void testIsMPLSWithEmptyResultOnIntentMappingService() throws Exception {
+        initIntentWithSourceAndDestination();
+
+        Map<String, String> map = new HashMap<>();
+        when(intentMappingService.get(Mockito.any(String.class))).thenReturn(map);
+
+        ofRendererFlowManagerProvider.isMPLS(intent);
+    }
+
+
+    @Test
+    public void testIsMPLSWithNoMappingForDestination() throws IntentInvalidException {
+        Map<String, String> mapSource = new HashMap<>();
+        mapSource.put(OFRendererConstants.MPLS_LABEL_KEY, "");
+
+        Map<String, String> mapDestination = new HashMap<>();
+        mapDestination.put(dstEndPointGroup.getName(), "");
+
+        when(intentMappingService.get(sourceEndPointGroup.getName())).thenReturn(mapSource);
+        when(intentMappingService.get(dstEndPointGroup.getName())).thenReturn(mapDestination);
+
+        Assert.assertFalse(ofRendererFlowManagerProvider.isMPLS(intent));
+    }
+
+    @Test
+    public void testIsMPLSWithNoMappingForSource() throws IntentInvalidException {
+        Map<String, String> mapSource = new HashMap<>();
+        mapSource.put(sourceEndPointGroup.getName(), "");
+
+        Map<String, String> mapDestination = new HashMap<>();
+        mapDestination.put(OFRendererConstants.MPLS_LABEL_KEY, "");
+
+        when(intentMappingService.get(sourceEndPointGroup.getName())).thenReturn(mapSource);
+        when(intentMappingService.get(dstEndPointGroup.getName())).thenReturn(mapDestination);
+
+        Assert.assertFalse(ofRendererFlowManagerProvider.isMPLS(intent));
+    }
+
+    @Test
+    public void testIsMPLS() throws IntentInvalidException {
+        Map<String, String> mapSource = new HashMap<>();
+        mapSource.put(OFRendererConstants.MPLS_LABEL_KEY, "");
+
+        Map<String, String> mapDestination = new HashMap<>();
+        mapDestination.put(OFRendererConstants.MPLS_LABEL_KEY, "");
+
+        when(intentMappingService.get(sourceEndPointGroup.getName())).thenReturn(mapSource);
+        when(intentMappingService.get(dstEndPointGroup.getName())).thenReturn(mapDestination);
+
+        Assert.assertTrue(ofRendererFlowManagerProvider.isMPLS(intent));
     }
 }
