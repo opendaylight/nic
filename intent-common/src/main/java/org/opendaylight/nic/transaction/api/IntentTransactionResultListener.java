1bd7913a986db1c0facf5d69d77452b8b8c9dff5
Yrineu Rodrigues <yrineu.rodrigues@serro.com>
2016-11-26 22:19:56 -0300

    Create RendererCommon model for integration
    Change-Id: I55b85de5ab07fcccc947f1e3364d55fa9451fbd1
    Signed-off-by: Yrineu Rodrigues <yrineu.rodrigues@serro.com>

--- intent-common/src/main/java/org/opendaylight/nic/transaction/api/IntentTransactionResultListener.java
+++ intent-common/src/main/java/org/opendaylight/nic/transaction/api/IntentTransactionResultListener.java
@@ -1,20 +1,16 @@
 /*
- * Copyright (c) 2016 Yrineu Rodrigues and others. All rights reserved.
+ * Copyright (c) 2016 Serro LCC. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.nic.transaction.api;
 
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.types.rev150122.Uuid;
-
-/**
- * Created by yrineu on 25/06/16.
- */
 public interface IntentTransactionResultListener {
 
-    void deployFailure(Uuid intentId);
+    void deployFailure(String intentId);
 
-    void deploySuccess(Uuid intentId);
+    void deploySuccess(String intentId);
 }
