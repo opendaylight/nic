b49a457861bcc8f8df0d61e0e0ba0e744ece8517
Yrineu Rodrigues <yrineu.rodrigues@serro.com>
2016-11-26 22:19:56 -0300

    Create RendererCommon model for integration
    Change-Id: I55b85de5ab07fcccc947f1e3364d55fa9451fbd1
    Signed-off-by: Yrineu Rodrigues <yrineu.rodrigues@serro.com>

--- intent-common/src/main/java/org/opendaylight/nic/common/transaction/api/IntentCommonProviderService.java
+++ intent-common/src/main/java/org/opendaylight/nic/common/transaction/api/IntentCommonProviderService.java
@@ -1,18 +1,15 @@
 /*
- * Copyright (c) 2016 Yrineu Rodrigues and others. All rights reserved.
+ * Copyright (c) 2016 Serro LCC. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.nic.transaction.api;
 
-import org.opendaylight.nic.transaction.TransactionResult;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.intent.types.rev150122.Uuid;
+package org.opendaylight.nic.common.transaction.api;
 
-/**
- * Created by yrineu on 07/07/16.
- */
+import org.opendaylight.nic.common.transaction.TransactionResult;
+
 public interface IntentCommonProviderService extends AutoCloseable {
 
     void start();
@@ -21,9 +18,9 @@ public interface IntentCommonProviderService extends AutoCloseable {
 
     void registerForResults(IntentTransactionResultListener resultListener);
 
-    void notifyResults(Uuid intentId, TransactionResult result);
+    void notifyResults(String intentId, TransactionResult result);
 
-    void notifyExecutors(Uuid intentId);
+    void notifyExecutors(String intentId);
 
     void unregisterForExecutor(IntentTransactionResultListener listener);
 
