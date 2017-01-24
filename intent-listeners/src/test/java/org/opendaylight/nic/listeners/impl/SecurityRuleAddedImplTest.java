64d7624e604221bfcbfc7a39171e6f211259ecff
Victor Mota <victor_mota@atlantico.com.br>
2017-01-24 11:15:27 -0300

    Increase intent-listeners coverage on SecurityRuleAdded.
    Change-Id: Ibcfb73b33af5e0b64ad41f929278317031065a74
    Signed-off-by: Victor Mota <victor_mota@atlantico.com.br>

--- intent-listeners/src/test/java/org/opendaylight/nic/listeners/impl/SecurityRuleAddedImplTest.java
+++ intent-listeners/src/test/java/org/opendaylight/nic/listeners/impl/SecurityRuleAddedImplTest.java
@@ -10,24 +10,15 @@ package org.opendaylight.nic.listeners.impl;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.opendaylight.nic.listeners.api.SecurityRuleAdded;
-import org.opendaylight.nic.listeners.api.SecurityRuleUpdated;
 import org.opendaylight.nic.neutron.NeutronSecurityRule;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.secgroups.rev150712.security.rules.attributes.security.rules.SecurityRule;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import utils.SecurityGroupUtils;
 import utils.SecurityRuleUtils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@PrepareForTest({SecurityGroupAddedImpl.class})
-@RunWith(PowerMockRunner.class)
 /**
  * Created by yrineu on 12/01/16.
  */
@@ -39,7 +30,7 @@ public class SecurityRuleAddedImplTest {
     @Before
     public void setUp() {
         SecurityRuleUtils.setUp();
-        securityRuleAdded = PowerMockito.spy(new SecurityRuleAddedImpl(SecurityRuleUtils.securityRuleMock));
+        securityRuleAdded = Mockito.spy(new SecurityRuleAddedImpl(SecurityRuleUtils.securityRuleMock));
         neutronSecurityRule = securityRuleAdded.getSecurityRule();
     }
 
