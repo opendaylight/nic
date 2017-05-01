d16d3b7938f940ed56398289d91b0df60411b4ac
yrineu <yrineu.rodrigues@serro.com>
2017-05-01 10:19:20 -0300

    BGP renderer - Initial structure
    Change-Id: I83e48e0585fbcaf6f89d7b198c7ae3657bc6c48a
    Signed-off-by: yrineu <yrineu.rodrigues@serro.com>

---
+++ bgp-renderer/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/nic/bgp/renderer/rev170501/BGPRendererProviderModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.nic.bgp.renderer.rev170501;
+public class BGPRendererProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.nic.bgp.renderer.rev170501.AbstractBGPRendererProviderModule {
+    public BGPRendererProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public BGPRendererProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.nic.bgp.renderer.rev170501.BGPRendererProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
