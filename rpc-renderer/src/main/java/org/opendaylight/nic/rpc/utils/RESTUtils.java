ea891046452ebff83bed750d2a3f6d1237a34c14
Yrineu Rodrigues <yrineu.rodrigues@serro.com>
2017-09-05 11:28:22 -0300

    EVPN operations. Part 1 of 2 - Retrieve mac learned for evpn configuration
    Change-Id: Ib8d158c77993b48ee2b32311526216796f45bbf7
    Signed-off-by: Yrineu Rodrigues <yrineu.rodrigues@serro.com>

--- rpc-renderer/src/main/java/org/opendaylight/nic/rpc/utils/RESTUtils.java
+++ rpc-renderer/src/main/java/org/opendaylight/nic/rpc/utils/RESTUtils.java
@@ -10,6 +10,10 @@ package org.opendaylight.nic.rpc.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Timestamp;
 
 /**
  * Created by yrineu on 17/07/17.
@@ -32,7 +36,41 @@ public class RESTUtils {
         return jsonNode.elements().next().get(DATA).asInt();
     }
 
+    public static Integer extractIntData(final JsonArray jsonArray) {
+        return jsonArray.get(0).getAsJsonObject().get(DATA).getAsInt();
+    }
+
+    public static Ipv4Address extractIpv4Address(final JsonArray jsonArray) {
+        return Ipv4Address.getDefaultInstance(getFirstData(jsonArray));
+    }
+
+    public static MacAddress extractMacAddress(final JsonArray jsonArray) {
+        return MacAddress.getDefaultInstance(getFirstData(jsonArray));
+    }
+
+    public static Timestamp extractTimeStamp(final JsonArray jsonArray) {
+        return Timestamp.getDefaultInstance(getFirstData(jsonArray));
+    }
+
+    private static String getFirstData(final JsonArray jsonArray) {
+        return jsonArray.get(0).getAsJsonObject().get(DATA).getAsString();
+    }
+
     public static boolean isEmptyNode(final JsonNode jsonNode) {
         return (jsonNode.equals(emptyNode));
+    }
+
+    public static String buildDeviceURLRequest(
+            final String httpIp,
+            final Integer httpPort,
+            final String requestedRPC) {
+        final StringBuffer url = new StringBuffer();
+        url.append("http://");
+        url.append(httpIp);
+        url.append(":");
+        url.append(httpPort);
+        url.append("/rpc/");
+        url.append(requestedRPC);
+        return url.toString();
     }
 }
--- rpc-renderer/src/main/test/org/opendaylight/nic/rpc/rest/RestValidationsTest.java
+++ rpc-renderer/src/main/test/org/opendaylight/nic/rpc/rest/RestValidationsTest.java
@@ -8,11 +8,22 @@
 
 package org.opendaylight.nic.rpc.rest;
 
+import com.google.common.collect.Sets;
+import com.google.gson.*;
+import com.google.gson.internal.bind.JsonTreeWriter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendaylight.nic.rpc.exception.JuniperRestException;
+import org.opendaylight.nic.rpc.model.juniper.information.device.SwitchInformation;
+import org.opendaylight.nic.rpc.model.juniper.information.evpn.DatabaseInfo;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Timestamp;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by yrineu on 19/07/17.
@@ -21,17 +32,17 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 public class RestValidationsTest {
 
-    @Test (expected = JuniperRestException.class)
+    @Test(expected = JuniperRestException.class)
     public void testNullRequest() {
         RestValidations.validateReceivedRequest(null);
     }
 
-    @Test (expected = JuniperRestException.class)
+    @Test(expected = JuniperRestException.class)
     public void testInvalidRequest() {
         RestValidations.validateReceivedRequest("/192.1681111123::");
     }
 
-    @Test (expected = JuniperRestException.class)
+    @Test(expected = JuniperRestException.class)
     public void testRequestWithInvalidIp() {
         RestValidations.validateReceivedRequest("/nic/192.166666");
     }
@@ -39,5 +50,54 @@ public class RestValidationsTest {
     @Test
     public void testWithValidRequest() {
         RestValidations.validateReceivedRequest("/nic/192.168.1.1");
+    }
+
+    @Test
+    public void test() {
+
+        final JsonParser parser = new JsonParser();
+        final JsonElement element = parser.parse(getMockResultAsString());
+        final JsonElement cosElement = element.getAsJsonObject().get("evpn-database-information");
+//        System.out.println(element.getAsJsonObject().get("interface-cos-summary").toString());
+        final Iterator<JsonElement> iterator = cosElement.getAsJsonArray().iterator();
+        while (iterator.hasNext()) {
+            JsonElement el = iterator.next();
+//            System.out.println(el.getAsJsonObject().get("intf-cos-forwarding-classes-supported").getAsJsonArray().get(0).getAsJsonObject().get("data"));
+        }
+//
+//        DatabaseInfo databaseInfo = new DatabaseInfo(
+//                100,
+//                MacAddress.getDefaultInstance("00:00:00:00:00:01"),
+//                Ipv4Address.getDefaultInstance("192.168.1.1"),
+//                Ipv4Address.getDefaultInstance("10.0.0.1"));
+////        System.out.println(getMockResultAsString());
+//
+//        SwitchInformation switchInformation = new SwitchInformation(Ipv4Address.getDefaultInstance("192.168.1.1"), 300, "Serro", "1234");
+//        Gson gson = new Gson();
+////        System.out.println(gson.toJson(switchInformation));
+////        switchInformation = new SwitchInformation(switchInformation, databaseInfo);
+//        System.out.println(result);
+
+        Set<DatabaseInfo> jsons = Sets.newConcurrentHashSet();
+        DatabaseInfo databaseInfoA = new DatabaseInfo("SwitchA", "f4:15:63:7e:84:25");
+        DatabaseInfo databaseInfoB = new DatabaseInfo("SwitchB", "f4:15:63:7e:84:35");
+
+        databaseInfoA.extractEvpnInfoToJson(getMockResultAsString());
+        databaseInfoB.extractEvpnInfoToJson(getMockResultAsString());
+        jsons.add(databaseInfoA);
+        jsons.add(databaseInfoB);
+
+        Gson res = new Gson();
+        System.out.println(res.toJson(jsons));
+    }
+
+    public String getMockResultAsString() {
+        return "{\"evpn-database-information\":[{\"evpn-database-instance\":[{\"attributes\":{\"xmlns\":" +
+                "\"http://xml.juniper.net/junos/17.3R1/junos-routing\",\"junos:style\":\"normal\"},\"instance-name\"" +
+                ":[{\"data\":\"default-switch\"}],\"mac-entry\":[{\"attributes\":{\"xmlns\":" +
+                "\"http://xml.juniper.net/junos/17.3R1/junos-routing\",\"junos:style\":\"normal\"}," +
+                "\"vni-id\":[{\"data\":\"999\"}],\"mac-address\":[{\"data\":\"f4:15:63:7e:84:25\"}]," +
+                "\"active-source\":[{\"data\":\"10.200.19.21\"}],\"active-source-timestamp\":[{\"data\":" +
+                "\"Sep 01 21:43:01\"}],\"ip-address\":[{\"data\":\"10.132.252.8\"}]}]}]}]}";
     }
 }
